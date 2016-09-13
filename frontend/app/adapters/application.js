791ec4c18d668e575b73c4296b73c875ce49ddc7
Gaire, Raj (Data61, Acton) <Raj.Gaire@csiro.au>
2016-09-13 15:41:18 +1000

    deleted old version

--- frontend/app/adapters/application.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.RESTAdapter.extend({
-  host: 'http://localhost:1776/api/v1'
-  //host: 'http://dav127.it.csiro.au:1776/api/v1'
-});
--- frontend/app/controllers/mapview/maps.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  mapviewController: Ember.inject.controller('mapview')
-});
