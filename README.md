8f2689fb8152f688fd639d949dc5b9bf6ca01afa
Gabriel Keeble-Gagnere <gabriel7@gmail.com>
2016-09-15 20:18:28 +1000

    Rename map model to mapset and update mapview route accordingly. Can now list actual maps. Add comments.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Dav127
 Develop marker map viewer
 
-The database API codes (under the api folder) developed using sailsjs, and the web codes are in map_visualization folder. 
-The idea is to convert the current web codes into a form of reusable APIs using emberjs (in the front_end folder).  
+The database API codes (under the server folder) developed using sailsjs, and the web codes are in map_visualization folder. 
+The idea is to convert the current web codes into a form of reusable APIs using emberjs (in the frontend folder).  
 
 The entire framework is using seedsjs, which combines emberjs, sailsjs, semantic-UI together. 
-
-To start the server, simply run “seeds s” following the instructions on http://docs.seedsjs.com/
--- frontend/app/routes/mapview.js
+++ frontend/app/routes/mapview.js
@@ -3,11 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   queryParams: {
     mapsToView: {
+      // We want changes in the URL mapsToView query parameter to trigger
+      // a model refresh.
       refreshModel: true
     }
   },
 
   serializeQueryParam: function(value, urlKey, defaultValueType) {
+    // This serializes what gets passed to the link-to query parameter.
+    // Without it, an array simply gets JSON.stringified, for example:
+    // [1,2] becomes "[1,2]" and treated as a string.
     if (defaultValueType === 'array') {
       return value;
     }
@@ -26,7 +31,7 @@ export default Ember.Route.extend({
     console.log("params to mapview route:");
     console.log(params);
     // Get all available maps.
-    var maps = this.get('store').findAll('map');
+    var maps = this.get('store').findAll('mapset');
     this.controllerFor("mapview").set("availableMaps", maps);
     maps.then(function(maplist){
       maplist.forEach(function(map) {
@@ -44,7 +49,7 @@ export default Ember.Route.extend({
     for (var i=0; i < params.mapsToView.length; i++) {
       console.log("in mapview route loop:");
       console.log(params.mapsToView[i]);
-      retMaps.push(this.get('store').findRecord('map', params.mapsToView[i]));
+      retMaps.push(this.get('store').findRecord('mapset', params.mapsToView[i]));
     }
     return retMaps;
   }
