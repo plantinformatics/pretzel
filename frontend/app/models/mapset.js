2cae65892d62e25c0a4c4c7968f2a541ce73e1b8
Gabriel Keeble-Gagnere <gabriel7@gmail.com>
2016-09-15 21:35:36 +1000

    Simplify mapview menus and disable maps already selected.

--- frontend/app/models/mapset.js
+++ frontend/app/models/mapset.js
@@ -7,6 +7,14 @@ export default DS.Model.extend({
   linkTo: Ember.computed('name', function() {
     return [this.get("id")];
   }),
+  isSelected: Ember.computed('extraMaps', function() {
+    // Return true if this mapset has already been selected;
+    // in other words, if it is contained in the extraMaps array.
+    if (this.get("extraMaps").indexOf(this.get("id")) != -1) {
+      return true;
+    }
+    return false;
+  }),
   mapLink: Ember.computed('extraMaps', function() {
     var retlist = this.get("extraMaps");
     if (retlist == null) {
