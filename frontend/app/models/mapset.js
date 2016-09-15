24e654510f39b43c77098a299a5fd5bdd538ac01
Gabriel Keeble-Gagnere <gabriel7@gmail.com>
2016-09-15 20:20:38 +1000

    Add new mapset model file (replacing map model).

---
+++ frontend/app/models/mapset.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  extraMaps: [],
+  linkTo: Ember.computed('name', function() {
+    return [this.get("id")];
+  }),
+  mapLink: Ember.computed('extraMaps', function() {
+    var retlist = this.get("extraMaps");
+    if (retlist == null) {
+      return [this.get("id")];
+    }
+    else {
+      retlist.push(this.get("id"));
+      return retlist;
+    }
+  })
+
+});
