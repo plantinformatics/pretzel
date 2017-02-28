plantinformatics/pretzel

commit 5fa5e293f33c34e0285ffe33f7d467a5878472cf

Calculate the map dimensions from the browser viewport, factorise related calcs, refine the .stackDropTarget styling. draw-map.js: Update the margins m[] and use them in calcs. Add marginIndex to symbol-ise indices of m[]. Replace (in calculation of w and h)  view dimensions 1200,700 with document.documentElement.clientWidth,clientHeight. Calculate : wRange, yRange, and use throughout in place of w, h, respectively. #holder>svg : Use m[marginIndex.top],left in place of 100,100 in translate() and factor into translateTransform. Comment out use of (unimplemented) deleteMap(), and insert trace. Factor some code into : inRange(), markerY(), markerYk[]. Fix 'unused var' warnings by commenting out vars brushActives[] and reset, and some unused parameters (d). Factor -50,770 as dragLimit.min,max.
app.css :
 .stackDropTarget : cursor: move -> copy, comment out (no effect) : color, border, border-radius;
 refine : fill, stroke, stroke-dasharray.

 7249790  1892 Feb 22 10:28 frontend/app/styles/app.css
 9af5cac 24113 Feb 23 10:16 frontend/app/components/draw-map.js

frontend/app/components/draw-map.js
@@ -35,9 +35,21 @@ export default Ember.Component.extend({
 
 
     //margins, width and height (defined but not be used)
-    let m = [100, 160, 80, 320],
-    w = 1200 - m[1] - m[3],
-    h = 700 - m[0] - m[2];
+    let m = [100, 160, 80, 100],	// margins : top right bottom left
+    marginIndex = {top:0, right:1, bottom:2, left:3},	// indices into m[]; standard CSS sequence.
+    dropTargetYMargin = 10,
+		/** fit within margin left.
+		 * when the map name contains the 24 hex char mongodb numeric id, m[marginIndex.left]==100 fits well.
+		 */
+		dropTargetX = m[marginIndex.left]-1,
+		/// Width and Height.  viewport dimensions - margins.
+    w = document.documentElement.clientWidth	- m[marginIndex.right] - m[marginIndex.left],
+    h = document.documentElement.clientHeight - m[marginIndex.top] - m[marginIndex.bottom],
+		wRange = w*0.6,
+		yRange = h - 2 * dropTargetYMargin,
+		/// left and right limits of dragging the axes / chromosomes / linkage-groups.
+		dragLimit = {min:-50, max:wRange+70};
+		console.log("w=", w, ", h=", h, ", wRange=", wRange, ", yRange=", yRange);
 
     let y = {},
         copyY = {},
@@ -48,8 +60,14 @@ export default Ember.Component.extend({
     let line = d3.line(),
         axis = d3.axisLeft(),
         foreground,
-        brushActives = [],
+        // brushActives = [],
         brushExtents = [];
+
+		/// @return true if a is in the closed interval range[]
+		function inRange(a, range)
+		{
+			return range[0] <= a && a <= range[1];
+		}
 
     //Convert the data into proper format
     //myMaps mapset ID
@@ -71,12 +89,12 @@ export default Ember.Component.extend({
     });
     //d3 v4 scalePoint replace the rangePoint
     //let x = d3.scaleOrdinal().domain(mapIDs).range([0, w]);
-    let x = d3.scalePoint().domain(mapIDs).range([0, w]);
+    let x = d3.scalePoint().domain(mapIDs).range([0, wRange]);
     let o = {};
 
     let zoomSwitch,resetSwitch;
     let zoomed = false;
-    let reset = false;
+    // let reset = false;
 
     let pathMarkers = {}; //For tool tip
 
@@ -105,31 +123,33 @@ export default Ember.Component.extend({
     mapIDs.forEach(function(d) {
       y[d] = d3.scaleLinear()
                .domain([0,d3.max(Object.keys(z[d]), function(a) { return z[d][a]; } )])
-               .range([0, h]); // set scales for each map
+               .range([0, yRange]); // set scales for each map
       
       copyY[d] = d3.scaleLinear()
                 .domain([0,d3.max(Object.keys(z[d]), function(a) { return z[d][a]; } )])
-                .range([0, h]); 
+                .range([0, yRange]); 
 
       //console.log("OOO " + y[d].domain);
       y[d].flipped = false;
       y[d].brush = d3.brushY()
-                     .extent([[-8,0],[8,h]])
+                     .extent([[-8,0],[8,yRange]])
                      .on("end", brushended);
     });
 
     d3.select("svg").remove();
     d3.select("div.d3-tip").remove();
+		let translateTransform = "translate(" + m[marginIndex.left] + "," + m[marginIndex.top] + ")";
     let svgContainer = d3.select('#holder').append('svg')
-                         .attr('width',1200)
-                         .attr('height',700)
+                         .attr('width',w)
+                         .attr('height',h)
                          .append("svg:g")
-                         .attr("transform", "translate(100,100)");
+                         .attr("transform", translateTransform);
 
     //User shortcut from the keybroad to manipulate the maps
     d3.select("#holder").on("keydown", function() {
       if ((String.fromCharCode(d3.event.keyCode)) == "D") {
-        deleteMap();
+				console.log("Delete Map (not implemented)");
+        // deleteMap();
       }
       else if ((String.fromCharCode(d3.event.keyCode)) == "Z") {
         zoomMap();
@@ -183,10 +203,10 @@ export default Ember.Component.extend({
 			g.append("g")
 			.attr("class", "stackDropTarget")
 			.append("rect")
-      .attr("x", -100)
-      .attr("y", -10)
-      .attr("width", 150)
-      .attr("height", 100)
+      .attr("x", -dropTargetX)
+      .attr("y", -dropTargetYMargin)
+      .attr("width", 2 * dropTargetX)
+      .attr("height", 80)
 		;
 		stackDropTarget
       .on("mouseover", dropTargetMouseOver)
@@ -259,7 +279,7 @@ export default Ember.Component.extend({
          .style("top", (d3.event.pageY - 28) + "px");
     }
 
-    function handleMouseOut(d){
+    function handleMouseOut(/*d*/){
       let t = d3.transition()
                 .duration(800)
                 .ease(d3.easeElastic);
@@ -317,21 +337,31 @@ export default Ember.Component.extend({
         for (let k=0; k<mapIDs.length-1; k++) {
            //y[p].domain
            //z[mapIDs[k]][d] marker location
-           //y[mapIDs[k]](z[mapIDs[k]][d]) relative marker location in the map
+
+					/// Calculate relative marker location in the map
+					function markerY(k, d)
+					{
+						return y[mapIDs[k]](z[mapIDs[k]][d]);
+					}
             if (d in z[mapIDs[k]] && d in z[mapIDs[k+1]]) { // if markers is in both maps
-              //Remove those paths that either side locates out of the svg
-                  if(y[mapIDs[k]](z[mapIDs[k]][d]) <=h && y[mapIDs[k+1]](z[mapIDs[k+1]][d]) <=h 
-                      && y[mapIDs[k]](z[mapIDs[k]][d]) >=0 && y[mapIDs[k+1]](z[mapIDs[k+1]][d])>=0){
-                        let sLine = line([[o[mapIDs[k]], y[mapIDs[k]](z[mapIDs[k]][d])],
-                             [o[mapIDs[k+1]], y[mapIDs[k+1]](z[mapIDs[k+1]][d])]]);
+							/** relative marker location in the map of 2 markers, k and k+1 :
+							 * k  : markerYk[0]
+							 * k+1: markerYk[1]
+							 */
+							let markerYk = [markerY(k, d), markerY(k+1, d)];
+              // Filter out those paths that either side locates out of the svg
+              if (inRange(markerYk[0], yRange) &&
+									inRange(markerYk[1], yRange)) {
+                        let sLine = line([[o[mapIDs[k]], markerYk[0]],
+                             [o[mapIDs[k+1]], markerYk[1]]]);
                         if(pathMarkers[sLine] != null){
                           pathMarkers[sLine][d] = 1;
                         } else {
                           pathMarkers[sLine]= {};
                           pathMarkers[sLine][d] = 1;
                         }
-                        r.push(line([[o[mapIDs[k]], y[mapIDs[k]](z[mapIDs[k]][d])],
-                             [o[mapIDs[k+1]], y[mapIDs[k+1]](z[mapIDs[k+1]][d])]]));
+                        r.push(line([[o[mapIDs[k]], markerYk[0]],
+                             [o[mapIDs[k+1]], markerYk[1]]]));
                   } 
               
             } 
@@ -511,7 +541,7 @@ export default Ember.Component.extend({
     }
 
 
-    function dragstarted(d) {
+    function dragstarted(/*d*/) {
       d3.select(this).classed("active", true);
       d3.event.subject.fx = d3.event.subject.x;
     }
@@ -520,7 +550,8 @@ export default Ember.Component.extend({
       o[d] = d3.event.x;
       // Now impose boundaries on the x-range you can drag.
       // These values should really be based on variables defined previously.
-      if (o[d] < -50) { o[d] = -50; } else if (o[d] > 770) { o[d] = 770; }
+      if (o[d] < dragLimit.min) { o[d] = dragLimit.min; }
+			else if (o[d] > dragLimit.max) { o[d] = dragLimit.max; }
       mapIDs.sort(function(a, b) { return o[a] - o[b]; });
       //console.log(mapIDs + " " + o[d]);
       d3.select(this).attr("transform", function() {return "translate(" + o[d] + ")";});
@@ -541,14 +572,14 @@ export default Ember.Component.extend({
       svgContainer.selectAll("circle").remove();
     }
 
-    function dragended(d) {
+    function dragended(/*d*/) {
       // Order of mapIDs may have changed so need to redefine x and o.
-      x = d3.scalePoint().domain(mapIDs).range([0, w]);
+      x = d3.scalePoint().domain(mapIDs).range([0, wRange]);
       
       mapIDs.forEach(function(d){
         o[d] = x(d);
       });
-      x.domain(mapIDs).range([0, w]);
+      x.domain(mapIDs).range([0, wRange]);
       if(zoomed){
         d3.selectAll(".foreground g").selectAll("path").data(zoomPath).enter().append("path");  
       } else {
@@ -570,7 +601,7 @@ export default Ember.Component.extend({
      if (y[d].flipped) {
          y[d] = d3.scale.linear()
               .domain([0,d3.max(Object.keys(z[d]), function(x) { return z[d][x]; } )])
-              .range([0, h]); // set scales for each map
+              .range([0, yRange]); // set scales for each map
           y[d].flipped = false;
           var t = d3.transition().duration(500);
           t.selectAll("#"+d).select(".axis")
@@ -580,7 +611,7 @@ export default Ember.Component.extend({
       else {
           y[d] = d3.scale.linear()
               .domain([0,d3.max(Object.keys(z[d]), function(x) { return z[d][x]; } )])
-              .range([h, 0]); // set scales for each map
+              .range([yRange, 0]); // set scales for each map
           y[d].flipped = true;
           var t = d3.transition().duration(500);
           t.selectAll("#"+d).select(".axis")

frontend/app/styles/app.css
@@ -82,16 +82,18 @@ div.toolTip{
   border: 0px;
   border-radius: 2px;
 }
-/******************************************************************************/
+/* -------------------------------------------------------------------------- */
+/* target zone for axis stacking drag&drop
+ */
 .stackDropTarget {
-    cursor: move;
-    color: red;
-    border: 2px;
-    border-radius: 5px;
+    cursor: copy;
+    // color: red;
+    // border: 2px;
+    // border-radius: 5px;
 
-    fill: blue;
-    stroke: red;
-    stroke-width:2px;
-    stroke-dasharray: 2,2;
+    fill: rgba(80, 80, 255, 0.2);
+    stroke: rgb(255, 80, 80);
+    // stroke-width:1px;
+    stroke-dasharray: 3,8;
 }
-/******************************************************************************/
+/* -------------------------------------------------------------------------- */

commit 5eb9787b5963c4245f428108c7ac6c06ec2e4b38

Fixed zoom path (broke this in 5fa5e29); wrap with [0, ] the arg yRange passed to inRange(), regression test : brush an axis section containing a marker with a path (i.e. marker is in range on both axes), path should display.  e94724f 29710 Feb 28 15:55 frontend/app/components/draw-map.js

frontend/app/components/draw-map.js
@@ -118,7 +118,11 @@ chromosome : >=1 linkageGroup-s layed out vertically:
         // brushActives = [],
         brushExtents = [];
 
-    /// @return true if a is in the closed interval range[]
+    /**
+     * @return true if a is in the closed interval range[]
+     * @param a value
+     * @param range array of 2 values - limits of range.
+     */
     function inRange(a, range)
     {
       return range[0] <= a && a <= range[1];
@@ -502,8 +506,8 @@ chromosome : >=1 linkageGroup-s layed out vertically:
                */
               let markerYk = [markerY(k, d), markerY(k+1, d)];
               // Filter out those paths that either side locates out of the svg
-              if (inRange(markerYk[0], yRange) &&
-                  inRange(markerYk[1], yRange)) {
+              if (inRange(markerYk[0], [0, yRange]) &&
+                  inRange(markerYk[1], [0, yRange])) {
                         let sLine = line([[o[mapIDs[k]], markerYk[0]],
                              [o[mapIDs[k+1]], markerYk[1]]]);
                         if(pathMarkers[sLine] != null){