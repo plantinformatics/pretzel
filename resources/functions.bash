5048b671bd585861c1c82f8125e84757d89efd4b
Don <Don.Isdale@gmail.com>
2017-03-22 12:09:57 +1100

    resources/: added example_map5.json and modified map4, to create a simple geometric pattern between 2 maps. These 2 are created from the commands examples in : functions.bash: added markerPositions() to create marker name&position data (could generate these 2 data files from script instead of holding in git).  5a67f71     1115 Mar 22 11:39 resources/example_map4.json  302c28d     1124 Mar 22 11:47 resources/example_map5.json  d0eb10e     3882 Mar 22 11:47 resources/functions.bash

--- resources/functions.bash
+++ resources/functions.bash
@@ -74,6 +74,40 @@ Created new geneticmap
 
 Expected_output_delimiter
 
+#-------------------------------------------------------------------------------
+# Create marker position data for loading via load_test_data_file;
+# this is the content of {geneticmap { chromosomes {[ markers [ ... ] ]} }}.
+# Input is 2 lines: markerName-s then corresponding marker positions.
+# markerName-s are alphanumeric
+# marker positions are list of real numbers
+# values are separated by whitespace.
+#
+# A trailing comma is placed after each output;  the last one should be removed if it is placed before }.
+# Usage e.g.
+#   ( echo marker{A,B,C,D,E,F} ; echo 0.{1,2,3,4,5,6} ) | markerPositions
+#   ( echo marker{G,H,I,J,K,L} ; echo 0.{7,8,9} 1.{0,1,2}) | markerPositions
+#
+# a separate map, related to the above with a simple pattern :
+#   (head -9 resources/example_map4.json | sed s/MyMap4/MyMap5/;
+#   ( echo marker{B,C,D,E,F,A} ; echo 0.{1,2,3,4,5,6} ) | markerPositions
+#   ( echo marker{L,G,H,I,J,K} ; echo 0.{7,8,9} 1.{0,1,2}) | markerPositions
+#   tail -6 resources/example_map4.json) > resources/example_map5.json
+markerPositions()
+{
+    read -a markerNames
+    read -a markerPositions
+    len=${#markerNames[@]}
+    lastIndex=`expr $len - 1`
+
+    for i in 0 `yes '' | head -$lastIndex | cat -n`
+    do
+	echo "\
+          {
+          \"name\": \"${markerNames[$i]}\",
+          \"position\": ${markerPositions[$i]}
+          },"
+    done
+}
 
 #-------------------------------------------------------------------------------
 
