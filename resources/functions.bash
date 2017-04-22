408e1196cf2ec14bab43cfde3c40f04e0688901a
Don <Don.Isdale@gmail.com>
2017-04-22 21:54:21 +1000

    Add functions to generate example maps with a range of aliases group sizes. Test results : these 2 added maps show the alias uniqueness is not correctly determined by the current code. add argument  markerAliases to markerPositions(), add aliasGroups(), removeLastLineComma(), aliases_6(), aliases_7().  c3998fc 6198 Apr 22 21:33 resources/functions.bash

--- resources/functions.bash
+++ resources/functions.bash
@@ -84,6 +84,8 @@ Expected_output_delimiter
 #
 # A trailing comma is placed after each output;  the last one should be removed if it is placed before }.
 # Usage e.g.
+# (these examples are outdated by the addition of aliases; if loader requires aliases then these can be
+#  changed, otherwise markerPositions() can take an option to enable aliases. )
 #   ( echo marker{A,B,C,D,E,F} ; echo 0.{1,2,3,4,5,6} ) | markerPositions
 #   ( echo marker{G,H,I,J,K,L} ; echo 0.{7,8,9} 1.{0,1,2}) | markerPositions
 #
@@ -92,22 +94,89 @@ Expected_output_delimiter
 #   ( echo marker{B,C,D,E,F,A} ; echo 0.{1,2,3,4,5,6} ) | markerPositions
 #   ( echo marker{L,G,H,I,J,K} ; echo 0.{7,8,9} 1.{0,1,2}) | markerPositions
 #   tail -6 resources/example_map4.json) > resources/example_map5.json
+#
+# Examples with aliases :
+#  see aliases_6,7
+#
 markerPositions()
 {
     read -a markerNames
     read -a markerPositions
+    read -a markerAliases
     len=${#markerNames[@]}
     lastIndex=`expr $len - 1`
 
     for i in 0 `yes '' | head -$lastIndex | cat -n`
     do
+	ma=${markerAliases[$i]}
+	if [ -z "$ma" ]
+	then
+	    aliasGroups=
+	else
+	    aliasGroups=\"`eval echo $ma | sed 's/ /","/g'`\"
+	fi
 	echo "\
           {
           \"name\": \"${markerNames[$i]}\",
-          \"position\": ${markerPositions[$i]}
+          \"position\": ${markerPositions[$i]},
+          \"aliases\": [$aliasGroups]
           },"
     done
 }
+
+# used as input to markerPositions (the 3rd line - aliases)
+# Generate alias groups with 0,1,2,3,4,5 aliases respectively.
+# usage :
+# echo 'marker{a,b,c,d,e,f}' | aliasGroups
+# The input contains {,} which is expanded in markerPositions.
+aliasGroups()
+{
+    read -a markerNames
+    (
+	while echo "$markerNames" | fgrep -q , 
+	do
+	    echo "$markerNames"
+	    markerNames=`echo $markerNames | sed "s/,[A-Za-z]}/}/"`
+	done
+	markerNames=`echo $markerNames | sed "s/{//g;s/}//g"`
+	echo "$markerNames"
+	echo
+    ) | tac | tr '\n' ' '
+    echo
+}
+
+# remove ',' from the end of the last line.
+# JSON expects no comma before ].
+# markerPositions generates a comma after each repeated part (marker).
+function removeLastLineComma()
+{
+    sed '$s/^\( *}\),$/\1/'
+}
+
+example_template=resources/example_map4.json
+# example_map{6,7}.json are based on example_map{4,5}.json, with alias groups added to each marker
+function aliases_6()
+{
+    mapN=6
+    (head -9 $example_template | sed s/MyMap4/MyMap$mapN/;
+     # omitting from alias groups : ,c and ,j
+     # This is because we want an empty alias group.
+     ( echo marker{A,B,C,D,E,F} ; echo 0.{1,2,3,4,5,6} ; echo 'marker{a,b,d,e,f}' | aliasGroups ) | markerPositions
+     ( echo marker{G,H,I,J,K,L} ; echo 0.{7,8,9} 1.{0,1,2} ; echo 'marker{g,h,i,k,l}' | aliasGroups ) | markerPositions | removeLastLineComma
+     tail -5 $example_template)  > resources/example_map$mapN.json
+}
+
+function aliases_7()
+{
+    mapN=7
+    (head -9 $example_template | sed s/MyMap4/MyMap$mapN/;
+     # omitting from alias groups : ,C and ,J
+     ( echo marker{b,c,d,e,f,a} ; echo 0.{1,2,3,4,5,6} ; echo 'marker{B,D,E,F,A}' | aliasGroups ) | markerPositions
+     ( echo marker{l,g,h,i,j,k} ; echo 0.{7,8,9} 1.{0,1,2} ; echo 'marker{L,G,H,I,K}' | aliasGroups ) | markerPositions | removeLastLineComma
+     tail -6 $example_template) > resources/example_map$mapN.json
+}
+
+
 
 #-------------------------------------------------------------------------------
 
