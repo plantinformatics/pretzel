5c9cec38967d50b9cf89c20ee06fdd53647ffc9f
David Carroll <david.carroll@csiro.au>
2017-08-23 15:15:52 +1000

    Added string replace for line return in email chunking

--- backend/test/helpers/smtp.js
+++ backend/test/helpers/smtp.js
@@ -11,21 +11,16 @@ const server = new SMTPServer({
       callback(null, {user: 123}); // where 123 is the user id or similar property
     },
     onData(stream, session, callback){
-      // console.log('ONDATA')
-      let chunklen = 0;
       let chunks = ''
       let scope = this;
       
       stream.on('data', chunk => {
-          chunklen += chunk.length;
           chunk = String(chunk).trim()
-          // console.log(chunk)
-          // console.log(chunk.length)
+          chunk = chunk.replace(/=\r/g, '')
           chunks += chunk
       });
 
       stream.on('end', () => {
-          // console.log('CHUNKS', chunklen)
           callback();
           scope.emit('data', chunks)
       });
