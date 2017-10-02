edddc657e57b3a9515a4c4353a6016a161b81d54
Don <Don.Isdale@gmail.com>
2017-10-02 18:24:22 +1100

    Start changing the text area (content-editable) into a table, with tabs for the various spreadsheet inputs, including the display function of selected-markers div#grid. Add table-brushed.js, tabbed-table.hbs, table-brushed.hbs. Add handsontable, which is also added in other commits 066b1022 and c7cf18f. frontend/   5556ce2    3492 Oct  2 16:39 app/templates/mapview.hbs   52ba308     731 Oct  2 16:39 bower.json   6f9d4b0    1582 Oct  2 09:36 ember-cli-build.js   156eb0e    2040 Oct  2 16:39 app/components/table-brushed.js   e92e732    1310 Oct  2 15:00 app/templates/components/tabbed-table.hbs   9bb3985     155 Oct  2 09:36 app/templates/components/table-brushed.hbs

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -40,6 +40,8 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
   app.import('bower_components/d3/d3.js');
+  app.import('bower_components/handsontable/dist/handsontable.full.min.js');
+  app.import('bower_components/handsontable/dist/handsontable.full.min.css');
   app.import('vendor/js/divgrid/divgrid.js');
 
   return app.toTree();
