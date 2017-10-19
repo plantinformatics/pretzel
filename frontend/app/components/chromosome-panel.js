8f83df29e1d1d4d578a572469bf24e324d78ee5e
Alex Kruger <alex.kruger@csiro.au>
2017-10-19 16:05:50 +1100

    yep

--- frontend/app/components/chromosome-panel.js
+++ frontend/app/components/chromosome-panel.js
@@ -3,9 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   chr: Ember.computed('selectedChrom', function() {
     let chrom = this.get('selectedChrom')
-    if (chrom) {
-      console.log(chrom.get('markers'))
-    }
     return chrom;
   }),
   markers: Ember.computed('chr', function() {
