273b35c05092ebc975e17fe89dcfb8f90b0e78c9
Don <Don.Isdale@gmail.com>
2018-06-13 12:07:56 +1000

    Handle either feature.range or .value currently have a mix of both in pretzel-data [develop] (myMap.json has value [0, 0] and public_maps/json/pbi0012-0787-SD23.fixed.json has range [0])  b88b983  499 Jun 13 12:04 frontend/app/models/feature.js  650db6b 2012 Jun 13 12:04 frontend/app/utils/utility-chromosome.js

--- frontend/app/utils/utility-chromosome.js
+++ frontend/app/utils/utility-chromosome.js
@@ -1,3 +1,5 @@
+import { breakPoint } from '../utils/breakPoint';
+
 /*----------------------------------------------------------------------------*/
 
 /** bundle chr data (incl features) for draw-map:draw().
@@ -23,11 +25,19 @@ function chrData(c) {
   f.forEach(function(feature) {
     let featureName = feature.get('name');
     /** range should be defined and be an array, but this will handle a mix of
-     * data from other source versions without exception. */
-    let range = feature.get('value'), // was range
+     * data from other source versions without exception.
+     *
+     * Also field name is changing feature.range <-> .value
+     * Current data is mixed format  :
+     * from pretzel-data [develop] : myMap.json has value [0, 0] and 
+     * public_maps/json/pbi0012-0787-SD23.fixed.json has range [0]
+     */
+    let range = feature.get('value') || feature.get('range'),
     featurePosition = range && range[0];
     let featureAliases = feature.get('aliases');  // feature.aliases field is removed from db
     let featureId = feature.get('id');
+    if (featurePosition === undefined)
+      breakPoint('chrData', c, map, rc, f, feature, range, featurePosition, featureAliases, featureId);
     rc[featureName] = {location: featurePosition, aliases: featureAliases, id: featureId};
     if (!range) console.log("chrData range", featureName, rc[featureName]);
   });
