8857bd98c873afc25d290e67befaa99caafb1e54
Don <Don.Isdale@gmail.com>
2018-07-25 14:11:53 +1000

    Change path of optional customisable included html file
    {logosEtc,landingPageContent}.html
     c4e69bc  1580 Jul 25 14:07 frontend/app/controllers/index.js

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -6,12 +6,13 @@ import DS from 'ember-data';
 /** path of site-specific HTML file to insert into the landing page.
  * For 'ember server' this is relative to frontend/public/
  */
-let siteSpecificHtmlUrl = 'logosEtc.html';
+let siteSpecificHtmlUrl = 'landingPageContent.html';
 /** HTML to show in place of the site-specific file if there is none.  */
 let alternateHtml =
   "<!-- To allow use-specific customisation, content can be loaded into this \
 position, from a file separate from the source repository. The html is loaded \
-from this path, if available : frontend/public/logosEtc.html -->";
+from this file : " + siteSpecificHtmlUrl + ", if available; when using 'ember server' \
+it will be in this path : frontend/public/ -->";
 
 export default Ember.Controller.extend({
 
@@ -24,13 +25,13 @@ export default Ember.Controller.extend({
           url: siteSpecificHtmlUrl,
           method: 'GET',
           success: function (html) {
-            console.log('logosEtc', html.length);
+            console.log(siteSpecificHtmlUrl, html.length);
             resolve(html);
             // me.set('logosEtcProperty', html);
           },
           error: function (jqXHR, statusText, error) {
             let callbackContext = this;
-            console.log('logosEtc file not read', siteSpecificHtmlUrl,
+            console.log('Optional customisable html file not read', siteSpecificHtmlUrl,
             'statusText=', statusText, 'error=', error, 'url=', callbackContext.url);
             resolve(alternateHtml);
             // reject();
