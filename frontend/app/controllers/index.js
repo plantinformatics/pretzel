80a123f0e374e4121cc1268fb335367b64233d4e
Don <Don.Isdale@gmail.com>
2018-07-27 19:00:30 +1000

    Load landingPageContent.html in place of the index.hbs default content, instead of after it.
    Return message + alternateHtml in reject reason.
    frontend/app/
     38bd4d4 1714 Jul 27 18:53  controllers/index.js
     c02e0b7  585 Jul 27 18:51  templates/index.hbs

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -5,18 +5,21 @@ import DS from 'ember-data';
 
 /** path of site-specific HTML file to insert into the landing page.
  * For 'ember server' this is relative to frontend/public/
+ * The attribute name of the promise and the file base name need not be the same.
  */
 let siteSpecificHtmlUrl = 'landingPageContent.html';
-/** HTML to show in place of the site-specific file if there is none.  */
+/** HTML to show in place of the site-specific file if there is none.
+ * The template can wrap this with e.g. <div style="display:none;">...</div>.
+ */
 let alternateHtml =
-  "<!-- To allow use-specific customisation, content can be loaded into this \
+  "To allow use-specific customisation, content can be loaded into this \
 position, from a file separate from the source repository. The html is loaded \
 from this file : " + siteSpecificHtmlUrl + ", if available; when using 'ember server' \
-it will be in this path : frontend/public/ -->";
+it will be in this path : frontend/public/ ";
 
 export default Ember.Controller.extend({
 
-  logosEtc: Ember.computed( function () {
+  landingPageContent: Ember.computed( function () {
     let me = this;
     return DS.PromiseObject.create({
       promise:
@@ -27,14 +30,13 @@ export default Ember.Controller.extend({
           success: function (html) {
             console.log(siteSpecificHtmlUrl, html.length);
             resolve(html);
-            // me.set('logosEtcProperty', html);
           },
           error: function (jqXHR, statusText, error) {
-            let callbackContext = this;
-            console.log('Optional customisable html file not read', siteSpecificHtmlUrl,
-            'statusText=', statusText, 'error=', error, 'url=', callbackContext.url);
-            resolve(alternateHtml);
-            // reject();
+            let callbackContext = this,
+            message = 'Optional customisable html file ' + siteSpecificHtmlUrl
+             + ' "' + error + '"' + ', url=' + callbackContext.url;
+            console.log(message, 'error=', error);
+            reject(message + ".\n      " + alternateHtml);
           }
         });
       })
