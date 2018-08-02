93700d045a09a09551ba24669f77ae21cf9cad55
Don <Don.Isdale@gmail.com>
2018-08-02 19:48:14 +1000

    Make inclusion of landingPageContent html work for ember build also.
    middleware.json : files: expose /client/landingPageContent as loopback#static.
    controllers/index : add a text input field landingPageContentFileName for experimenting with path of static landingPageContent files, without having to rebuild; just a devel aid, not essential.
    landingPageContent() : changed to use a simpler and possibly newer form of ajax call; this is equivalent, not a functional change.
     9cc4040    918 Aug  2 18:22 backend/server/middleware.json
     ca1c696   1978 Aug  2 17:35 frontend/app/controllers/index.js
     4273f0c    798 Aug  2 18:03 frontend/app/templates/index.hbs

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -6,41 +6,54 @@ import DS from 'ember-data';
 /** path of site-specific HTML file to insert into the landing page.
  * For 'ember server' this is relative to frontend/public/
  * The attribute name of the promise and the file base name need not be the same.
+ * Loopback will map this to a directory by appending /, and load index.html from that directory.
  */
-let siteSpecificHtmlUrl = 'landingPageContent.html';
+const siteSpecificHtmlUrl = 'landingPageContent/index.html';
 /** HTML to show in place of the site-specific file if there is none.
  * The template can wrap this with e.g. <div style="display:none;">...</div>.
  */
-let alternateHtml =
+const alternateHtml =
   "To allow use-specific customisation, content can be loaded into this \
 position, from a file separate from the source repository. The html is loaded \
 from this file : " + siteSpecificHtmlUrl + ", if available; when using 'ember server' \
 it will be in this path : frontend/public/ ";
 
 export default Ember.Controller.extend({
+  ajax: Ember.inject.service(),
 
-  landingPageContent: Ember.computed( function () {
+  landingPageContentFileName : siteSpecificHtmlUrl,
+
+  landingPageContent: Ember.computed('landingPageContentFileName', function () {
     let me = this;
-    return DS.PromiseObject.create({
-      promise:
-      new Ember.RSVP.Promise(function (resolve, reject) {
-        Ember.$.ajax({
-          url: siteSpecificHtmlUrl,
-          method: 'GET',
-          success: function (html) {
-            console.log(siteSpecificHtmlUrl, html.length);
-            resolve(html);
-          },
-          error: function (jqXHR, statusText, error) {
-            let callbackContext = this,
-            message = 'Optional customisable html file ' + siteSpecificHtmlUrl
-             + ' "' + error + '"' + ', url=' + callbackContext.url;
-            console.log(message, 'error=', error);
-            reject(message + ".\n      " + alternateHtml);
-          }
-        });
-      })
+    let fileUrl = this.get('landingPageContentFileName');
+    console.log('landingPageContentFileName', fileUrl);
+
+      let promise = this.get('ajax').request(fileUrl, {
+        method: 'GET',
+        dataType: 'html',
+        contentType: 'text/html; charset=utf-8',
+        'Accept': 'text/html'
+      });
+    promise.then(function (html) {
+      console.log(fileUrl, html.length);
+      /* <img> src will need path landingPageContent/, but perhaps better to edit the
+       * index.html when it is installed.  */
+      if (false) {
+        let imgs = Ember.$('.ember-view table img');
+        imgs.attr('src', function (index, val) { return val.replace(/^/, 'landingPageContent/'); });
+      }
     });
+    promise.catch(
+      function (/*jqXHR, statusText,*/ error) {
+        let callbackContext = this,
+        message = 'Optional customisable html file ' + fileUrl
+          + ' "' + error + '"' + ', url=' + callbackContext.url;
+        console.log(message, 'error=', error);
+        // reject(message + ".\n      " + alternateHtml);
+      }
+    );
+
+    return promise;
   })
 
 
