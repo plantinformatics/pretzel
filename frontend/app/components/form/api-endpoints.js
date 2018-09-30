be25d415f44b691402b52e23a366a12af1d7c897
Don <Don.Isdale@gmail.com>
2018-09-30 23:01:55 +1000

    Added components/form/api-endpoints.js
    This file should have been included in previous commit, [feature/multi-backend 4a56726]
     8007f05    640 Sep 24 19:29 frontend/app/components/form/api-endpoints.js

---
+++ frontend/app/components/form/api-endpoints.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+const { inject: { service }, Component } = Ember;
+
+export default Component.extend({
+  apiEndpoints: service('api-endpoints'),
+
+    addEmpty : function () {
+      this.get('apiEndpoints').addEndpoint(undefined, undefined, undefined);
+      console.log('addEmpty', this);
+    },
+
+
+  actions: {
+    addEmpty : function () {
+      console.log('action addEmpty', this);
+      this.addEmpty();
+    }
+
+  },
+
+  endpoints : Ember.computed('apiEndpoints.endpoints.@each', function () {
+    let endpoints = this.get('apiEndpoints.endpoints');
+    console.log('endpoints', endpoints, this);
+    return endpoints;
+  })
+
+});
