38e6c05f99389aa1aaf51f64eb51043d5fd087f9
Don <Don.Isdale@gmail.com>
2018-12-05 16:54:58 +1100

    Identify datasets without parents, and separate them into GMs and References.
    Change the data of the byDataset tab, from byDataset to dataWithoutParent, and split off -references : parents off to a new tab.
    move the length info below the tab to save width.
    CF byDataset() is replaced by added CFs : withParent, names, child1, parents, parentNames, withoutParent, withoutParentNames, parentsid, parentsNonUnique, parentsUnique, parentsUniqueNames, dataWithoutParentNames, parentsContent, dataWithoutParent, dataWithoutParent0Names, parentsSet, dataWithoutParent1.  These calculate dataWithoutParent via 2 methods, only 1 is required.  The *Names functions are for checking / devel trace.
    frontend/app/
     cf41d0e  6345 Dec  5 16:49  components/panel/manage-explorer.js
     6f68c41 20970 Dec  5 16:23  styles/app.css
     308cdd1  3411 Dec  5 16:20  templates/components/panel/manage-explorer.hbs

--- frontend/app/components/panel/manage-explorer.js
+++ frontend/app/components/panel/manage-explorer.js
@@ -1,5 +1,7 @@
 import Ember from "ember";
 
+import { filter, filterBy, mapBy, setDiff, uniqBy } from '@ember/object/computed';
+
 import ManageBase from './manage-base'
 
 /* global d3 */
@@ -50,18 +52,73 @@ export default ManageBase.extend({
     if (availableMaps && availableMaps.length > 0) { return false; }
     else { return true; }
   }),
+  /* ------------------------------------------------------------------------ */
   /** group the data in : Dataset / Block
    * Used for datasets without a parent
    */
-  byDataset : Ember.computed('data', function() {
-    let datasets = this.get('data'),
-    noParent = datasets.filter(function(f) { 
-      let p = f.get('parent');
-      console.log('byDataset', f, p, p.get('content'));
-      return ! p.get('content');
-    });
-    return noParent;
+
+  /** datasets with a .parent, i.e. containing child data blocks */
+  withParent: filter('data', function(dataset, index, array) {
+    return dataset.get('parent.content');
   }),
+  /** Names of all datasets - just for trace / devel, not used. */
+  names : mapBy('data', 'name'),
+
+  // parents are reference assemblies
+  /** result of uniqBy is a single dataset which refers to each (parent)
+   * dataset; just 1 child of each parent is in the result. */
+  child1 : uniqBy('withParent', 'parent.name'),
+  /** parents of child1(), i.e. all the parent datasets, just once each.  */
+  parents : mapBy('child1', 'parent'),
+  /** names of parents(). */
+  parentNames : mapBy('parents', 'name'),
+  /** Datasets without a .parent; maybe a reference assembly (genome) or a GM. */
+  withoutParent: filter('data', function(dataset, index, array) {
+    return ! dataset.get('parent.content');
+  }),
+  // these 3 CFs are non-essential, used in trace.
+  withoutParentNames : mapBy('withoutParent', 'name'),
+  parentsid : mapBy('withParent', 'parent.id'),
+  // an alternate method to calculate parents: parentsUnique
+  parentsNonUnique : mapBy('withParent', 'parent'),
+  parentsUnique : uniqBy('parentsNonUnique', 'name'),
+  /** for trace - checking parentsUnique(). */
+  parentsUniqueNames : mapBy('parentsUnique', 'name'),
+
+  // dataWithoutParent are genetic maps
+  /** setDiff works on names, but not on computed properties, hence the need for parentsContent.
+   * This works; only used as checking trace / devel.
+   */
+  dataWithoutParentNames : setDiff('withoutParentNames', 'parentNames'),
+  parentsContent : mapBy('parents', 'content'),
+  dataWithoutParent : setDiff('withoutParent', 'parentsContent'),
+  /** Same result as dataWithoutParentNames; just for trace / checking. */
+  dataWithoutParent0Names : mapBy('dataWithoutParent', 'name'),
+
+  /** used by dataWithoutParent1 (i.e. the non-setDiff version) */
+  parentsSet : Ember.computed('parentNames', function () {
+    let parents = this.get('parentNames'),
+    set = parents.reduce(function(result, value) { return result.add(value); }, new Set());
+    return set;
+  }),
+  /** Alternative to dataWithoutParent based on setDiff.
+   * The setDiff version is working OK, after introducing parentsContent,
+   * so this equivalent is not required.
+   */
+  dataWithoutParent1: filter('withoutParent', function(dataset, index, array) {
+    let parentsSet = this.get('parentsSet'),
+    name = dataset.get('name'),
+    found = parentsSet.has(name);
+    if (index === 0)
+    {
+      console.log('dataWithoutParent', array, parentsSet, dataset);
+    }
+    console.log(dataset._internalModel.__data, index, name, found);
+    return ! found;
+  }).property('withoutParent.[]', 'parentsSet', 'parents'),
+
+  /* ------------------------------------------------------------------------ */
+
   /** group the data in : Parent / Scope / Block
    */
   dataTree : Ember.computed('data', function() {
