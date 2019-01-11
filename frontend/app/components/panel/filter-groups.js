c9311b7655c2c09ea84f184b4af5f18feb083e97
Don <Don.Isdale@gmail.com>
2019-01-11 18:20:02 +1100

    Move filter-groups from Datasets container to Filters.
    frontend/app/
     3db4f58   549 Jan 11 11:14  components/panel/filter-groups.js
     f37e3e2 21507 Jan 11 11:14  styles/app.css
     1462bc9  4250 Jan 11 10:51  templates/components/panel/manage-explorer.hbs

--- frontend/app/components/panel/filter-groups.js
+++ frontend/app/components/panel/filter-groups.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  classNames : ["filter-groups"],
+
   actions : {
     addFilterOrGroup : function () {
       console.log('addFilterOrGroup');
