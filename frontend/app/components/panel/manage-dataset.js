64746cd479a20d34a594b7773eafc98e1677fdd8
Kieran Lomas <kieran.lomas@csiro.au>
2019-01-16 15:45:04 +1100

    Include a JSON editor for metadata

--- frontend/app/components/panel/manage-dataset.js
+++ frontend/app/components/panel/manage-dataset.js
@@ -1,9 +1,16 @@
 import ManageBase from './manage-base';
 
+
+/** @param: dataset **/
+
 export default ManageBase.extend({
-  
   actions: {
-    
+    mutateJson(json) {
+      this.set("dataset.meta", json)
+      console.log('this.get("dataset.meta") => ', this.get("dataset.meta"))
+
+    }
+
   },
   
 });
