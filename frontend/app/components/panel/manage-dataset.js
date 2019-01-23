151aa0dcd3b0ec3d5e2899da1a9faf0d01947072
Kieran Lomas <kieran.lomas@csiro.au>
2019-01-23 15:27:13 +1100

    Initialise JSON to an empty object, editor didn't like undefined

--- frontend/app/components/panel/manage-dataset.js
+++ frontend/app/components/panel/manage-dataset.js
@@ -4,6 +4,9 @@ import ManageBase from './manage-base';
 /** @param: dataset **/
 
 export default ManageBase.extend({
+  datasetMeta: Ember.computed("dataset.meta", function() {
+    return this.get("dataset.meta") || {}
+  }),
   actions: {
     mutateJson(json) {
       this.set("dataset.meta", json)
@@ -14,7 +17,6 @@ export default ManageBase.extend({
     saveToDB() {
       this.get("dataset").save()
     }
-
   },
   
 });
