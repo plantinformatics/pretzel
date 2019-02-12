2895fdeda04cfab97cb4857e636d5a1ffa805b15
Don <Don.Isdale@gmail.com>
2019-02-12 12:03:01 +1100

    Add alias2(), blockFeaturesSet() in doc/mongo_functions_alias.js.
    These are present in a further progressed form in backend/common/utilities/paths-aggr.js in later stash 3ee4dbd, which will be popped and labelled features/perf_mongo_aggregate_draft2
    Continuing drafting of mongo aggregate()-s to improve performance.
    From stash : 806c5b0, Date:   Mon Nov 5 12:49:59 2018 +1100
     based on 2nd .aggregate from alias1()
     developed 31 October  14:48 - 2018-10-31T05:21:57
    
    e4eb491  2996 Feb 12 11:46 doc/mongo_functions_alias.js

--- doc/mongo_functions_alias.js
+++ doc/mongo_functions_alias.js
@@ -1,3 +1,9 @@
+/*----------------------------------------------------------------------------*/
+
+/* globals defined in mongo shell */
+/* global db ObjectId print */
+/*----------------------------------------------------------------------------*/
+
 function alias1(n) {
 
 var b = db.Block.aggregate ( [
@@ -19,3 +25,49 @@ var a = db.Block.aggregate ( [
 var i = 0;  a.forEach ( function (a0) {a0.aliased_features.forEach(function (f) { if (bf.has(f) && (i++ < 10)) { print(f);  } }); });
 print(i);
 }
+
+/*----------------------------------------------------------------------------*/
+/*
+ * based on 2nd .aggregate from alias1()
+ * developed 31 October  14:48 - 2018-10-31T05:21:57
+ * Usage e.g. var a = alias2("5b7f8afd43a181430b81394d", 3)
+ */
+function alias2(blockId, n) {
+ var a = db.Block.aggregate ( [
+ { $match : { "_id" : ObjectId(blockId) } },
+ {$lookup: { from: 'Feature', localField: '_id', foreignField: 'blockId', as: 'featureObjects' }}, {$unwind: '$featureObjects' }, { $limit: n }
+, { $lookup: { from: "Alias", localField: "featureObjects.name", foreignField: "string1", as: "feature_aliases" } }
+, { "$project": { "_id" : 0, "feature_aliases": { "$slice": ["$feature_aliases", n] },
+    "_id" : 1,
+    "scope" : 1,
+    "name" : 1,
+    "namespace" : 1,
+    "datasetId" : 1,
+    "featureType" : 1,
+    "featureObjects" : 1
+ } }
+//, {$unwind: '$feature_aliases' }, { $limit: n }
+]);
+
+  return a;
+}
+
+/*----------------------------------------------------------------------------*/
+
+/**
+ * Usage e.g. var bfs = blockFeaturesSet("5b7f8afd43a181430b81394e", 3)
+ */
+function blockFeaturesSet(blockId, n) {
+	// based on first half of alias1()
+	var b = db.Block.aggregate ( [
+		{ $match : { "_id" : ObjectId(blockId) } },
+		{$lookup: { from: 'Feature', localField: '_id', foreignField: 'blockId', as: 'featureObjects' }}, {$unwind: '$featureObjects' }, { $limit: n }
+		, { $group: { _id: null, features : { $addToSet: "$featureObjects.name" } }   }
+	] );
+
+	var bf = new Set();
+	b.forEach ( function (b0) {b0.features.forEach(function (f) { bf.add(f); }); });
+	return bf;
+}
+
+/*----------------------------------------------------------------------------*/
