e867059462157153284d0e09dd8712caea3a269c
Don <Don.Isdale@gmail.com>
2019-02-18 14:04:01 +1100

    Add some sys admin function.
    resources/tools/
     e4c482d  639 Jan 22 19:43 logDateTime.bash
     fd11fb9  751 Jan 22 19:50 mongo_admin.bash

---
+++ resources/tools/mongo_admin.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Usage :
+#  source ~/scripts/logDateTime.bash
+#  source ~/scripts/mongo_admin.bash
+
+dbCollections()
+{
+    if [ -z "$DIM" ];
+     then 
+	 echo DIM : Docker identity of Mongo Instance is required
+	 return 1
+    else
+	docker exec -it $DIM mongo --quiet admin --eval "db.getCollectionNames()" | tr -d '[\[\]",]'
+    fi
+}
+
+
+function mongodump2S3()
+{
+  logDate=`date +%Y%b%d`
+  echo $logDate
+  # 2018Sep26
+  export S3_MON="s3://shared-data-4pretzel/mongodb/main.admin/$logDate/"
+  echo $S3_MON
+  collections=$(dbCollections )
+  echo $collections
+  sleep 5
+
+  # AccessToken Alias Block Client Dataset Feature 
+  for i in $collections; do echo $i; time  mongodump  --db admin --collection $i  -o -  | gzip -c | aws s3 cp - $S3_MON/$i.gz; done
+}
