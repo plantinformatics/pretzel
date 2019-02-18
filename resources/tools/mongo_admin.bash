67debb066ce36b05093621c27d7c12bcbbe82ad0
Don <Don.Isdale@gmail.com>
2019-02-18 14:23:53 +1100

    fixes in script functions dbCollections() and mongodump2S3()
     73afcf1 767 Feb 18 14:11 resources/tools/mongo_admin.bash

--- resources/tools/mongo_admin.bash
+++ resources/tools/mongo_admin.bash
@@ -11,7 +11,7 @@ dbCollections()
 	 echo DIM : Docker identity of Mongo Instance is required
 	 return 1
     else
-	docker exec -it $DIM mongo --quiet admin --eval "db.getCollectionNames()" | tr -d '[\[\]",]'
+	docker exec -it $DIM mongo --quiet admin --eval "db.getCollectionNames()" | tr -d '[\[\]",\i ]' | tr '\r' ' '
     fi
 }
 
@@ -21,7 +21,7 @@ function mongodump2S3()
   logDate=`date +%Y%b%d`
   echo $logDate
   # 2018Sep26
-  export S3_MON="s3://shared-data-4pretzel/mongodb/main.admin/$logDate/"
+  export S3_MON="s3://shared-data-4pretzel/mongodb/main.admin/$logDate"
   echo $S3_MON
   collections=$(dbCollections )
   echo $collections
