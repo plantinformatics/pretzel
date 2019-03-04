d84403d493994e498913db1c323bb6e2ee27928b
Kieran Lomas <kieran.lomas@csiro.au>
2019-03-04 16:29:18 +1100

    clean up cache code

--- backend/common/models/block.js
+++ backend/common/models/block.js
@@ -44,71 +44,62 @@ module.exports = function(Block) {
     let db = this.dataSource
     let blockCollection = db.connector.collection("Block");
 
-    let cacheId0 = blockId0 + ":" + blockId1
-    let cacheId1 = blockId1 + ":" + blockId0
+    let cacheId = blockId0 > blockId1
+        ? blockId1 + ":" + blockId0
+        : blockId0 + ":" + blockId1
+
+    console.log("blockId0", blockId0)
+    console.log("blockId1", blockId1)
+    console.log('blockId0 > blockId1 => ', blockId0 > blockId1);
+    console.log('cacheId => ', cacheId);
+    
     let array = []
-    let isCached = false
 
     console.log('cache => ', cache);
 
-    if(cache[cacheId0] !== undefined) {
-      array = cache[cacheId0]
-      isCached = true
-      console.log("Found in cache with id ", cacheId0);
+    if(cache[cacheId] !== undefined) {
+      array = cache[cacheId]
+      console.log("Found in cache with id ", cacheId);
       cb(null, array)
+      return
     }
-    else if(cache[cacheId1]) {
-      array = cache[cacheId1]
-      isCached = true
-      console.log("Found in cache with id ", cacheId1);
+
+    console.log("Not found in cache: ", cacheId);
+    var cursor = blockCollection.aggregate ( [
+      { $match :  {
+          $or : [{ "_id" : ObjectId(blockId0) },
+                 { "_id" : ObjectId(blockId1) }]
+        }
+      },
+      { $lookup: { from: 'Feature', localField: '_id', foreignField: 'blockId', as: 'featureObjects' }},
+      { $unwind: '$featureObjects' }, 
+      // { $limit: 5 }
+      { $group: { 
+          _id: {name : '$featureObjects.name', blockId : '$featureObjects.blockId'},
+          features : { $push: '$featureObjects' },
+          // count: { $sum: 1 }
+        }
+      },
+      { $group: {
+          _id: { name: "$_id.name" },
+          alignment: { $push: { blockId: '$_id.blockId', repeats: "$features"}}
+        }
+      },
+      { $match : { alignment : { $size : 2 } }}
+    ])
+    // res.pipe(cursor)
+    cursor.on('data', doc => {
+      // console.log('doc => ', doc);
+      array.push(doc)
+    })
+
+    cursor.on('end', () => {
+      // console.log('array => ', array);
+      cache[cacheId] = array
       cb(null, array)
-    }
-
-    
-    // console.log("blockId0", blockId0)
-    // console.log("blockId1", blockId1)
-
-    if(!isCached) { 
-      console.log("Not found in cache: ", cacheId0, " or ", cacheId1);
-      var cursor = blockCollection.aggregate ( [
-        { $match :  {
-            $or : [{ "_id" : ObjectId(blockId0) },
-                   { "_id" : ObjectId(blockId1) }]
-          }
-        },
-        { $lookup: { from: 'Feature', localField: '_id', foreignField: 'blockId', as: 'featureObjects' }},
-        { $unwind: '$featureObjects' }, 
-        // { $limit: 5 }
-        { $group: { 
-            _id: {name : '$featureObjects.name', blockId : '$featureObjects.blockId'},
-            features : { $push: '$featureObjects' },
-            // count: { $sum: 1 }
-          }
-        },
-        { $group: {
-            _id: { name: "$_id.name" },
-            alignment: { $push: { blockId: '$_id.blockId', repeats: "$features"}}
-          }
-        },
-        { $match : { alignment : { $size : 2 } }}
-      ])
-      // res.pipe(cursor)
-      cursor.on('data', doc => {
-        // console.log('doc => ', doc);
-        array.push(doc)
-      })
-
-      cursor.on('end', () => {
-        // console.log('array => ', array);
-        cache[cacheId0] = array
-        cb(null, array)
-      })
-      // var cursor = blockCollection.find()
-    }
-
-  
-
-    
+      return
+    })
+    // var cursor = blockCollection.find()    
     // cb(null, cursor, 'application/octet-stream');
     // cursor.on('data', function(doc) {
     //   console.log(doc);
