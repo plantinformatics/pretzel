eba3d26dbe0056e0fac30f33faf4a8799dab2f57
Don <Don.Isdale@gmail.com>
2019-03-14 19:31:10 +1100

    Add serializers/block-adj, with extractId()
    which was intended to be included in 7e568fe
      b6a1556 390 Mar 13 14:10 frontend/app/serializers/block-adj.js

---
+++ frontend/app/serializers/block-adj.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+  compositeKeys: ['block0', 'block1'],
+
+  extractId(modelClass, resourceHash) {
+    console.log('extractId', modelClass, resourceHash);
+    let i0 = this.compositeKeys.map((key) => {let i = resourceHash[key]; console.log('extractId', i); return i; }).join('-');
+  console.log('extractId', i0);
+  return i0;
+  }
+});
