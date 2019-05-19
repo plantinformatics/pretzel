plantinformatics/pretzel

commit ef915d1a79795a64c5c68c1094f49763978a156d

updating block domain after loading features from brush : handle some additional cases.
draw-map.js : brushHelper() : return when brushExtents[i] is undefined.
paths-progressive.js : requestBlockFeaturesInterval() : fix args to blocksUpdateDomain : changed to array of blockIds in ba5a24c;
if ! result then instead trace .state() of the promise (paths, pathsAliases, features).
stacks.js : add Block.prototype.{datasetHasParent,features,maybeDomainCalc}() : recognise when domain is uninitialised in maybeDomainCalc().
frontend/app/ :
 44aa0ed 213819 May 17 09:19  components/draw-map.js
 b6717c4  19334 May 16 16:30  services/data/paths-progressive.js
 910c231  63998 May 16 16:00  utils/stacks.js

frontend/app/components/draw-map.js
@@ -3576,9 +3576,22 @@ export default Ember.Component.extend(Ember.Evented, {
           /** compound name dataset:block (i.e. map:chr) for the selected axis p.  */
           let mapChrName = axisName2MapChr(p);
           selectedFeatures[mapChrName] = [];
-          let enable_log = brushExtents[i] === undefined;
+          let notBrushed = brushExtents[i] === undefined,
+          enable_log = notBrushed;
             if (enable_log)
             console.log("brushHelper", p, i);
+          /* brushExtents[i] is required for the following calculation of
+           * brushedDomain and hence the filtering, so return if it is undefined.
+           * The above selectedAxes.removeObject() is intended to prevent this but it seems to miss some case.
+           * And selectedAxes[] does not only derive from brushes - an axis can be selected in the data explorer.
+           * The whole flow of calculation in brushHelper() needs to be changed :
+           * it is unnecessary to traverse selectedAxes[] - only 1 axis has
+           * been brushed, and p is known via (thisElement.__data__, name[0], brushedAxisID).
+           * brushExtents[] is not required, and brushedRegions[] can be
+           * independent of selectedAxes[].
+           */
+          if (notBrushed)
+            return;
 
           // this can use axisRange2Domain() which is based on this function.
           let yp = oa.y[p],

frontend/app/utils/stacks.js
@@ -141,6 +141,19 @@ Block.prototype.isReference = function() {
     (axis.referenceBlock === blockR);
   return isReference;
 };
+/** @return truthy if the dataset of this Block has a parent.
+ * A genetic map block is a data block and has no reference block; for the purpose of stacks it is the reference.
+ * @return e.g. null if (dp is a promise and) no parent
+ * @see isData()
+ * @see isReference()
+ */
+Block.prototype.datasetHasParent = function() {
+  let dataset = this.block.get('datasetId'),
+  dp = dataset.get('parent'),
+  hasParent = dp && (dp.isPending ? dp.get('content') : dp);
+  console.log('datasetHasParent', dataset, dp, hasParent); this.log();
+  return hasParent;  
+};
 /** @return true if this Block is a data block, not the reference block.
  */
 Block.prototype.isData = function() {
@@ -148,7 +161,7 @@ Block.prototype.isData = function() {
   blockR = this.block,
   isData =
     //  checking if features is defined and features.length > 0
-    (blockR.get('namespace') || blockR.get('features.length'));
+    (blockR.get('namespace') || blockR.get('features.length') || ! this.datasetHasParent());
   return isData;
 };
 
@@ -437,11 +450,23 @@ Stacked.prototype.yRange2 = function ()
   range = this.position.map(function (p) { return yRange * p; });
   return range;
 };
+/** Access the features hash of this block.
+ * The hash (currently) contains some additional block attributes, .dataset and
+ * .scope, which are ignored using isOtherField[].
+ */
+Block.prototype.features = function ()
+{
+  // this function abstracts access to .z so that it can be re-structured.
+  let d = this.axisName,
+  /** this.z should have the value oa.z[d]. */
+  z = this.z || oa.z[d];
+  return z;
+}
 /** Calculate the domain of feature locations in the block named this.axisName.
  */
 Block.prototype.domainCalc = function ()
 {
-  let d = this.axisName, features = oa.z[d],
+  let d = this.axisName, features = this.features(),
   blockAttr = oa.cmName[d];
   function featureLocation(a)
   {
@@ -451,10 +476,22 @@ Block.prototype.domainCalc = function ()
     domain =
     (blockAttr && blockAttr.range) ||
     d3.extent(Object.keys(features), featureLocation);
-  // console.log("domainCalc", this, d, features, domain);
+   console.log("domainCalc", this, d, features, domain);
   if (! domain || ! domain.length)
     breakPoint();
   return domain;
+};
+/** If the Block domain has not been calculated, then calculate it.
+ * The domain should be re-calculated after features are added.
+ * @return  domain, same result as .domainCalc()
+ */
+Block.prototype.maybeDomainCalc = function ()
+{
+  let d = this.domain,
+  features = this.features();
+  if (! d || (d.length === 2 && d[0] === false && d[1] === false))
+    this.domain = this.domainCalc();
+  return this.domain;
 };
 /** Traverse the blocks displayed on this axis, and return a domain which spans
  * their domains.
@@ -463,7 +500,7 @@ Stacked.prototype.domainCalc = function ()
 {
   console.log('domainCalc', this, this.blocks);
   let blockDomains = 
-    this.blocks.map(function (b) { return b.domain || (b.domain = b.domainCalc()); }),
+    this.blocks.map(function (b) { return b.maybeDomainCalc(); }),
   /** refn : https://github.com/d3/d3-array/issues/64#issuecomment-356348729 */
   domain = 
     [

frontend/app/services/data/paths-progressive.js
@@ -102,7 +102,7 @@ export default Service.extend({
       }
       else
         paths = this.requestPathsProgressive(blockAdj, blockAdjId);
-      console.log('getPathsProgressive', blockAdj, blockAdjId, paths);
+      console.log('getPathsProgressive', blockAdj, blockAdjId, result || paths.state());
     }
     return paths;
   },
@@ -335,7 +335,7 @@ export default Service.extend({
       }
       else
         pathsAliases = this.requestAliases(blockAdj, blockAdjId);
-      console.log('getPathsAliasesProgressive', blockAdj, blockAdjId, pathsAliases);
+      console.log('getPathsAliasesProgressive', blockAdj, blockAdjId, result || pathsAliases.state());
     }
     return pathsAliases;
   },
@@ -464,7 +464,7 @@ export default Service.extend({
       }
       else
         features = this.requestBlockFeaturesInterval(blockId);
-      console.log(fnName, blockId, features);
+      console.log(fnName, blockId, result || features.state());
     }
     return features;
   },
@@ -522,7 +522,7 @@ export default Service.extend({
 
           Ember.run.throttle(
             me, me.blocksUpdateDomain, 
-            dataBlockIds, undefined, domainCalc, axisEvents,
+            dataBlockIds, domainCalc, axisEvents,
             200, false);
         };
     promise

commit 07bc9f87d632467ef2106259e632e022b15d7dbc

fixes and improvements re. paths requests driven by zooming
block-adj.js : wrap getPaths{,Aliases}Progressive calls in added getPaths() and use RSVP.allSettled() if both are used; use that in a task - added taskGetPaths(), with .drop() so that calls for each block-adj are rate-limited; in draw/block-adj : draw() : handle results from the combined promises.
paths-filter.js : densityFilter() : fix args to .slice().
draw-map.js : zoomBehavior : limit mousewheel zoom extent with .scaleExtent().
stacks.js : isData() : use isReference() in place of datasetHasParent(), which doesn't distinguish parents from GMs (the extension of isData in ef915d1 caused references to be included in adjAxes, which was harmless but issued unproductive requests).
 f5653e8  12474 May 19 17:10 backend/common/utilities/paths-filter.js
frontend/app/ :
 52f5fbf 213851 May 19 16:00  components/draw-map.js
 dd628d5  13323 May 17 16:52  components/draw/block-adj.js
 44e2ee4   5983 May 17 15:59  models/block-adj.js
 de4ba32  63993 May 17 16:52  utils/stacks.js

backend/common/utilities/paths-filter.js
@@ -77,7 +77,7 @@ function densityFilter(filteredPaths, intervals) {
   if (filteredPaths.length > intervals.nFeatures) {
     if (trace_filter)
       console.log(filteredPaths.length, '> nFeatures', intervals.nFeatures);
-    filteredPaths = filteredPaths.slice(intervals.nFeatures);
+    filteredPaths = filteredPaths.slice(0, (intervals.nFeatures < 0) ? 0 : intervals.nFeatures);
   }
   return filteredPaths;
 }

frontend/app/components/draw-map.js
@@ -1060,6 +1060,7 @@ export default Ember.Component.extend(Ember.Evented, {
           // console.log('zoom.filter', this, arguments, d3.event, include);
           return include; } )
         .wheelDelta(wheelDelta)
+        .scaleExtent([1e-8, 1])
         .on('zoom', zoom)
       ;
       console.log('zoomBehavior', oa.zoomBehavior);

frontend/app/components/draw/block-adj.js
@@ -127,6 +127,13 @@ export default Ember.Component.extend(Ember.Evented, AxisEvents, {
    * @param paths grouped by features
    */
   draw (featurePaths) {
+    /* If result of promises for both direct and aliases, then use just [0]
+     * which is the direct result, because draw() does not yet handle paths aliases format.
+     */
+    if (featurePaths.length && featurePaths[0].state) {
+      let direct = featurePaths[0];
+      featurePaths = direct.value;
+    }
     if (featurePaths.length === 0)
       return;
 

frontend/app/models/block-adj.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 import attr from 'ember-data/attr';
+import { task } from 'ember-concurrency';
 
 const { inject: { service } } = Ember;
 
@@ -106,11 +107,23 @@ export default DS.Model.extend({
     if (blockAdjId[0] === undefined)
       blockAdjId = this.id.split('_');
 
+    let result = this.get('taskGetPaths').perform(blockAdjId);
+    return result;
+  }),
+  /** Depending on flows.{direct,alias}.visible, call getPathsProgressive() and
+   * getPathsAliasesProgressive().
+   * Those functions may make a request to the backend server if a current result is not in hand,
+   * so this function is wrapped by taskGetPaths().
+   * @return promise of paths by either direct or alias connections.
+   */
+  getPaths : function (blockAdjId) {
+    let
+      result,
+    id = this.get('id');
     let flowsService = this.get('flowsService'),
     flows = flowsService.get('flows');
 
     let me = this;
-    let result;
 
     if (flows.direct.visible) {
       let
@@ -139,10 +152,24 @@ export default DS.Model.extend({
       );
       if (result === undefined)
         result = pathsAliases;
+      else
+        result = Ember.RSVP.allSettled([result, pathsAliases]);
     }
 
     return result;
-  })
+  },
+
+  /** Wrap getPaths() in a task, with .drop() to debounce requests to backend server.
+   * @return promise of paths by either direct or alias connections.
+   * @see getPaths()
+   */
+  taskGetPaths : task(function* (blockAdjId) {
+    let me = this, result =
+      yield this.getPaths(blockAdjId);
+    console.log('taskGetPaths', result);
+    return result;
+  }).drop()
+
 
   /*--------------------------------------------------------------------------*/
 

frontend/app/utils/stacks.js
@@ -161,7 +161,7 @@ Block.prototype.isData = function() {
   blockR = this.block,
   isData =
     //  checking if features is defined and features.length > 0
-    (blockR.get('namespace') || blockR.get('features.length') || ! this.datasetHasParent());
+    (blockR.get('namespace') || blockR.get('features.length') || ! this.isReference());
   return isData;
 };
 