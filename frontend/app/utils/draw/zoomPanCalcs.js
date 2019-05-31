a0f77ccf47da149790cf9059fce623ccba82e694
Don <Don.Isdale@gmail.com>
2019-05-31 16:55:39 +1000

    When mousewheel zoom/pan reaches reference limits, adjust to those limits.
    Factor from wheelNewDomain() to form subInterval(); move (local copy of inRange)
    Add constrainInterval{1,}().
     07fba5e 6114 May 31 16:32 frontend/app/utils/draw/zoomPanCalcs.js

--- frontend/app/utils/draw/zoomPanCalcs.js
+++ frontend/app/utils/draw/zoomPanCalcs.js
@@ -5,7 +5,58 @@
 const trace_zoom = 2;
 /*----------------------------------------------------------------------------*/
 
-/**
+/* copied from draw-map.js; this has already been split out of draw-map.js into
+ * utils/graph-maths.js in an unpushed branch (8fccbd3).
+ */
+function inRange(a, range)
+{
+  return range[0] <= a && a <= range[1];
+}
+
+/** Test if an array of values, which can be a pair defining an interval, is
+ * contained within another interval.
+ * @param values  an array of values to test
+ */
+function subInterval(values, interval) {
+      // use `some` so that search completes as soon as one value is found out of range.
+  let ok =
+    ! values.some(function (d) { return ! inRange(d, interval); });
+  return ok;
+}
+
+/** This handles 1 end of the interval for @see constrainInterval().
+ */
+function constrainInterval1(sub, interval, i) {
+  if ( ! inRange(sub[i], interval)) {
+    /** other end of the interval from i */
+    let j = 1 - i;
+    /** Add shift to sub[] */
+    let shift = interval[i] - sub[i];
+    sub[i] = interval[i];
+    sub[j] += shift;
+  }
+}
+/** Shift sub so that it fits within interval.
+ * The caller ensures that length of sub is < length of interval.
+ *
+ * @param sub array[2] numeric
+ * @param interval  array[2] numeric
+ */
+function constrainInterval(sub, interval) {
+  [0, 1].forEach(function (i) {
+    constrainInterval1(sub, interval, i);
+  });
+}
+
+
+/*----------------------------------------------------------------------------*/
+
+/** Calculate the domain resulting from a mousewheel action (WheelEvent), which
+ * is interpreted as a pan if the shift key is depressed, and a zoom otherwise.
+ *
+ * zoom : if interval > domainSize, set it to that limit
+ * zoom & pan : if one (either) end of newDomain is outside axisReferenceDomain, set it to that limit
+ *
  * Uses : d3.event,
  * d3.event.sourceEvent (! inFilter), d3.event.currentTarget (! inFilter && ! isPan)
  *
@@ -17,6 +68,11 @@ const trace_zoom = 2;
  */
 function wheelNewDomain(axis, axisApi, inFilter) {
   let yp = axis.y;
+  /** Access these fields from the DOM event : .shiftKey, .deltaY, .currentTarget.
+   * When called from zoom(), d3.event is the d3 wrapper around the event, and
+   * the DOM event is referenced by .sourceEvent,  whereas in zoomFilter()
+   * d3.event is the DOM event.
+   */
   let e = inFilter ? d3.event : d3.event.sourceEvent,
   isPan = e.shiftKey;
   if (trace_zoom > 1)
@@ -44,7 +100,6 @@ function wheelNewDomain(axis, axisApi, inFilter) {
   lowerZoom = domainSize > 1e6 ? 50 : domainSize / 1e5;
 
   let
-    /** looks like .sourceEvent is defined in zoom(); whereas in zoomFilter() d3.event.deltaY works. */
     deltaY = e.deltaY;
 
   if (trace_zoom > 1)
@@ -63,13 +118,7 @@ function wheelNewDomain(axis, axisApi, inFilter) {
         domain[1] + newInterval
       ];
     if (inFilter) {
-      // copied from draw-map.js
-      function inRange(a, range)
-      {
-        return range[0] <= a && a <= range[1];
-      }
-      // use `some` so that search completes as soon as one value is found out of range.
-      include = ! newDomain.some(function (d) { return ! inRange(d, axisReferenceDomain); });
+      include = subInterval(newDomain, axisReferenceDomain);
     }
     if (trace_zoom > 1)
       console.log(deltaY, delta, 'newInterval', newInterval, newDomain, include);
@@ -89,6 +138,11 @@ function wheelNewDomain(axis, axisApi, inFilter) {
     /** length of new domain. */
     newInterval = interval * deltaScale,
     rangeSize = range[1] - range[0];
+    if (newInterval > domainSize) {
+      console.log('limit newInterval', newInterval, domainSize);
+      newInterval = domainSize;
+    }
+
     newDomain = [
       // range[0] < rangeYCentre, so this first offset from centre is -ve
       centre + newInterval * (range[0] - rangeYCentre) / rangeSize,
@@ -107,6 +161,13 @@ function wheelNewDomain(axis, axisApi, inFilter) {
       console.log(rangeYCentre, rangeSize, 'centre', centre);
     if (trace_zoom > 1)
       console.log(deltaY, deltaScale, 'newInterval', newInterval, newDomain, include);
+  }
+  // if one (either) end of newDomain is outside axisReferenceDomain, set it to that limit
+  if (! subInterval(newDomain, axisReferenceDomain))
+  {
+    console.log('! subInterval', newDomain, axisReferenceDomain);
+    constrainInterval(newDomain, axisReferenceDomain);
+    console.log('result of constrainInterval', newDomain);
   }
 
 
