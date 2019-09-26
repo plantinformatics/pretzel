cd658e9cab5c380b0f4937b57b170b06379c72e0
Don <Don.Isdale@gmail.com>
2019-09-26 09:26:02 +0000

    Add report of signups from database, grouped according to emailVerified.
    mongo_admin.bash : use SERVER_NAME, default to main; factor checkDIM() out of dbCollections(); add signupList(), signupReport().
     b63f54b1 2432 Sep 26 09:16 UTC dev:~/pretzel/resources/tools/mongo_admin.bash

--- resources/tools/mongo_admin.bash
+++ resources/tools/mongo_admin.bash
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+#-------------------------------------------------------------------------------
+
 # Usage :
 #  source ~/scripts/logDateTime.bash
 #  source ~/scripts/mongo_admin.bash
 
+#-------------------------------------------------------------------------------
+
+unused=${SERVER_NAME=main}
+
+#-------------------------------------------------------------------------------
+
+checkDIM()
+{
+    if [ -n "$DIM" ];
+     then 
+	 status=$?
+    else
+	 status=$?
+	 echo DIM : Docker identity of Mongo Instance is required
+    fi
+    return $status
+}
 dbCollections()
 {
-    if [ -z "$DIM" ];
-     then 
-	 echo DIM : Docker identity of Mongo Instance is required
-	 return 1
-    else
+    checkDIM &&
 	docker exec -it $DIM mongo --quiet admin --eval "db.getCollectionNames()" | tr -d '[\[\]",\t ]' | tr '\r' ' '
-    fi
 }
 
 
@@ -21,7 +35,7 @@ function mongodump2S3()
   logDate=`date +%Y%b%d`
   echo $logDate
   # 2018Sep26
-  export S3_MON="s3://shared-data-4pretzel/mongodb/main.admin/$logDate"
+  export S3_MON="s3://shared-data-4pretzel/mongodb/$SERVER_NAME.admin/$logDate"
   echo $S3_MON
   collections=$(dbCollections )
   echo $collections
@@ -34,3 +48,38 @@ function mongodump2S3()
   && aws s3 ls $S3_MON.tar.gz	\
   && docker exec -i -e TERM=$TERM $DIM bash -c 'rm -r /dump/admin'
 }
+
+#-------------------------------------------------------------------------------
+
+
+# Output a list of signups, from the Client collection.
+#
+# @param 1	emailVerified : true or false, true matches if emailVerified exists in the Client record, false matches if it does not exist
+# default value : false.
+# This param can be provided via environment variable :
+# emailVerified=false signupList
+# or : signupList false
+function signupList()
+{
+    if [ $# -gt 0 ]
+    then
+       emailVerified=$1
+    fi
+    unused=${emailVerified=false}
+
+    checkDIM &&
+	docker exec -i $DIM mongo --quiet admin <<EOF
+db.Client.aggregate( [
+  { \$match : { emailVerified: { \$exists: $emailVerified }} },
+  { \$project: { _id: 0, signUp : {\$dateToString:{date:{\$toDate:"\$_id"}, format:"%Y-%m-%d %H:%M:%S %z", timezone : 'Australia/Melbourne'}}, email : 1, name : 1, institution : 1, project : 1, emailVerified : 1 } }
+])
+EOF
+}
+function signupReport()
+{
+  echo -e 'Email\tName\tInstitution\tProject\tSignUp'
+  signupList | egrep -v '\.Isdale' | jq -r 'map(.)  | @tsv'
+}
+
+
+#-------------------------------------------------------------------------------
