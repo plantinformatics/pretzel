a7f47ba064b4f72cec38b75aba7b5e4d8c07fde2
Don <Don.Isdale@gmail.com>
2019-09-26 14:35:18 +1000

    add --gzip to mongodump to fit in space available in dev container
     c10157b   952 Sep  4 13:32 from dev:~/scripts/mongo_admin.2019Sep04.bash (only part is staged)  resources/tools/mongo_admin.bash

--- resources/tools/mongo_admin.bash
+++ resources/tools/mongo_admin.bash
@@ -28,7 +28,7 @@ function mongodump2S3()
   sleep 5
 
   # AccessToken Alias Block Client Dataset Feature 
-  for i in $collections; do echo $i; time docker exec -it $DIM mongodump  --db admin --collection $i; done
+  for i in $collections; do echo $i; time docker exec -it $DIM mongodump --gzip --db admin --collection $i; done
 
   docker exec -i -e TERM=$TERM $DIM bash -c 'cd /dump/; tar zcf - admin' | aws s3 cp -  $S3_MON.tar.gz	\
   && aws s3 ls $S3_MON.tar.gz	\
