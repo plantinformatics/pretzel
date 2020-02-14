4b8605296c7abbde5c4cadbfbaad89164b95634c
Don <Don.Isdale@gmail.com>
2020-02-14 16:21:41 +1100

    Add block column to paths-table
    add checkboxes (.optionControls) : blockColumn, showDomains, showCounts
    Add block column, enabled by checkbox.
    Change setPair() -> setEndpoint() - optional param block.
    filterOut() : use brushOfBlock to indicate whether axis is brushed (since selectedFeaturesOfBlock may be empty even when brushed).
    Add : classNames : paths-tables.
    frontend/app/ :
     e9425c3  9873 Feb 14 16:16  components/panel/paths-table.js
     295f264 30793 Feb 14 16:03  styles/app.scss	(partially staged)
     c1c7192  1921 Feb 14 15:56  templates/components/panel/paths-table.hbs

--- frontend/app/components/panel/paths-table.js
+++ frontend/app/components/panel/paths-table.js
@@ -8,11 +8,23 @@ import { pathsResultTypes } from '../../utils/paths-api';
 
 const dLog = console.debug;
 
+/**
+ * Arguments passed to template :
+ * @param selectedFeatures  catenation of features within all brushed regions
+ * @param selectedBlock block selected via axis brush, or via click in dataset explorer or view panel, or adding the block.
+ *
+ * @desc inputs from template
+ * @param blockColumn true means show the block name:scope in a column instead of as a row.
+ */
 export default Ember.Component.extend({
   flowsService: service('data/flows-collate'),
   pathsPro : service('data/paths-progressive'),
   axisBrush: service('data/axis-brush'),
 
+  blockColumn : true,
+  showDomains : false,
+
+  classNames: ['paths-tables'],
 
   didReceiveAttrs() {
     this._super(...arguments);
@@ -68,6 +80,15 @@ export default Ember.Component.extend({
     let selectedBlock = this.get('selectedBlock');
     let selectedFeaturesByBlock = this.get('selectedFeaturesByBlock');
     let axisBrush = this.get('axisBrush');
+    /** true means show the block name:scope in a column instead of as a row. */
+    let blockColumn = this.get('blockColumn');
+    /** true means show the brushed domains in the table. This may be omitted, although it
+     * seems important to know if the axis was brushed and what the region
+     * was.  */
+    let showDomains = this.get('showDomains');
+    /** true means show counts of paths before and after filtering in the table.  */
+    let showCounts = this.get('showCounts');
+
 
     if (false) {
     /**   form is e.g. : {Chromosome: "myMap:1A.1", Feature: "myMarkerA", Position: "0"} */
@@ -84,35 +105,43 @@ export default Ember.Component.extend({
       // components/panel/manage-settings.js:14:        return feature.Block === selectedBlock.id
       if (! selectedBlock || blockAdj.adjacentTo(selectedBlock)) {
         /** Prepare a row with a pair of values to add to the table */
-        function setPair(row, i, feature, position) {
+        function setEndpoint(row, i, block, feature, position) {
+          if (block)
+            row['block' + i] = block;
           row['feature' + i] = feature;
           row['position' + i] = position;
           return row;
-        }
-        /** push the names of the 2 adjacent blocks, as an interleaved title row in the table. */
+        };
         let blocks = blockAdj.get('blocks'),
-        blocksById = blocks.reduce((r, b) => { r[b.get('id')] = b; return r; }, {}),
-        namesFlat = blocks.reduce(
-          (rowHash, b, i) =>
-            setPair(rowHash, i, b.get('datasetId.id'), b.get('scope')), {});
-        // blank row to separate from previous blockAdj
-        result.push({'feature0': '_'});
-        result.push(namesFlat);
-        /** Add to the table the brushed domain of the 2 blocks. */
-        let brushedDomains = blocks.reduce((rowHash, b, i) => {
-          let ab = axisBrush.brushOfBlock(b),
-          brushedDomain = ab && ab.get('brushedDomain');
-          if (brushedDomain) {
-            setPair(rowHash, i, brushedDomain[0], brushedDomain[1]);
-          }
-          return rowHash;
-        }, {});
-        result.push(brushedDomains);
+          blocksById = blocks.reduce((r, b) => { r[b.get('id')] = b; return r; }, {});
+        if (! blockColumn) {
+          /** push the names of the 2 adjacent blocks, as an interleaved title row in the table. */
+          let
+          namesFlat = blocks.reduce(
+            (rowHash, b, i) =>
+              setEndpoint(rowHash, i, undefined, b.get('datasetId.id'), b.get('scope')), {});
+          // blank row to separate from previous blockAdj
+          result.push({'feature0': '_'});
+          result.push(namesFlat);
+        }
+        if (showDomains) {
+          /** Add to the table the brushed domain of the 2 blocks. */
+          let brushedDomains = blocks.reduce((rowHash, b, i) => {
+            let ab = axisBrush.brushOfBlock(b),
+            brushedDomain = ab && ab.get('brushedDomain');
+            if (brushedDomain) {
+              setEndpoint(rowHash, i, undefined, brushedDomain[0], brushedDomain[1]);
+            }
+            return rowHash;
+          }, {});
+          result.push(brushedDomains);
+        }
 
         let resultElts = blockAdj.get(pathsResultField);
         if (resultElts) {
         if (resultElts.length)
-          result.push(setPair({}, 0, 'Paths loaded', resultElts.length));
+          if (showCounts)
+            result.push(setEndpoint({}, 0, undefined, 'Paths loaded', resultElts.length));
           let outCount = 0;
           resultElts.forEach((resultElt) => {
             /** for each end of the path, if the endpoint is in a brushed block,
@@ -122,6 +151,11 @@ export default Ember.Component.extend({
 
             /** for the given block, generate the name format which is used in
              * selectedFeatures.Chromosome
+             *
+             * block.get('datasetNameAndScope') may be the same value; it can
+             * use shortName, and its purpose is display, whereas
+             * selectedFeatures.Chromosome is for identifying the block (and
+             * could be changed to blockId).
              */
             function blockDatasetNameAndScope(block) {
               return block.get('datasetId.id') + ':' + block.get('scope');
@@ -151,12 +185,19 @@ export default Ember.Component.extend({
               chrName = blockDatasetNameAndScope(block),
               selectedFeaturesOfBlock = selectedFeaturesByBlock[chrName],
               featureName = featureGet('name'),
+              /** if the axis is brushed but there are no features in this block
+               * within the brush, then selectedFeaturesOfBlock will be
+               * undefined (empty).  If the axis is not brushed then no filter
+               * is applied on this endpoint.
+               */
+              isBrushed = !!axisBrush.brushOfBlock(block),
               out = selectedFeaturesOfBlock ?
                 /* endpoint feature is not in selectedFeaturesOfBlock */
                 ! selectedFeaturesOfBlock[featureName]
-                : false;
+                : isBrushed;
               if (! out) {
                 let value = featureGet('value');
+                path['block' + i] = block.get('datasetNameAndScope');
                 path['position' + i] = '' + value;
                 path['feature' + i] = featureName;
               }
@@ -168,7 +209,8 @@ export default Ember.Component.extend({
               outCount++;
             }
           });
-          result.push(setPair({}, 0, 'Filtered Count', outCount));
+          if (showCounts)
+            result.push(setEndpoint({}, 0, undefined, 'Filtered Count', outCount));
         }
       }
       return result;
@@ -182,6 +224,9 @@ export default Ember.Component.extend({
     'pathsAliasesResult.[]',
     'selectedBlock',
     'selectedFeaturesByBlock.@each',
+    'blockColumn',
+    'showDomains',
+    'showCounts',
     function () {
       let tableData = this.filterPaths('pathsAliasesResult');
       dLog('tableData', tableData);
@@ -191,6 +236,9 @@ export default Ember.Component.extend({
     'pathsResult.[]',
     'selectedBlock',
     'selectedFeaturesByBlock.@each',
+    'blockColumn',
+    'showDomains',
+    'showCounts',
     function () {
       let tableData = this.filterPaths('pathsResult');
       dLog('tableData', tableData);
