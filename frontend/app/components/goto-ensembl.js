1eed2da4500295f9b7c502075ad5aa181fdc69c4
Don <Don.Isdale@gmail.com>
2020-03-16 00:09:17 +1100

    Add Dawn to External Lookup.
    goto-ensembl.js : add axisBrush, classNames : goto-ensemble, add Dawn to websites, add baseUrls(), baseUrl(),
    call urlFunction with this (so far only used in urlDawn);  add urlLocation, brush(), brushedDomain(), urlDawn()
    
    goto-ensembl.hbs :  add input : baseUrl,  urlLocation (if brushedDomain).
    pass selectedBlock=selectedBlock from manage-search via left-panel and goto-feature to goto-ensembl.
    factor style (of div and a) to .scss
    app.scss : add style for .goto-ensemble,
    frontend/app/ :
     5bfb739  8288 Mar 15 23:56  components/goto-ensembl.js
     045695f 31867 Mar 15 18:39  styles/app.scss
     5924497   991 Mar 15 23:39  templates/components/goto-ensembl.hbs
     5b3e55f  1445 Mar 13 16:50  templates/components/goto-feature.hbs
     6127ef4  3074 Mar 13 16:50  templates/components/panel/left-panel.hbs
     df0492d   625 Mar 13 16:50  templates/components/panel/manage-search.hbs

--- frontend/app/components/goto-ensembl.js
+++ frontend/app/components/goto-ensembl.js
@@ -1,26 +1,84 @@
 import Ember from 'ember';
+const { inject: { service } } = Ember;
 
 import { breakPoint } from '../utils/breakPoint';
 
 /*----------------------------------------------------------------------------*/
 
 const trace_url = 0;
+const dLog = console.debug;
+dLog('goto-ensemble');
 
 /*----------------------------------------------------------------------------*/
 
 export default Ember.Component.extend({
+  axisBrush: service('data/axis-brush'),
+
+  classNames : [ 'goto-ensemble'],
 
   website: 'GrainGenes',
-  websites: Ember.String.w('GrainGenes Ensembl'),
+  websites: Ember.String.w('GrainGenes Ensembl Dawn'),
+  /** user may provide a configuration URL for each website,
+   * input via baseUrl. */
+  baseUrls : {},
+
   actions: {
     selectWebsite(website) {
       this.set('website', website);
     }
   },
 
+  /*--------------------------------------------------------------------------*/
+
+  baseUrl : Ember.computed('website', 'baseUrls.@each', {
+    get () {
+      return this.get('baseUrls')[this.get('website')];
+    },
+    set(key, value) {
+      let baseUrls = this.get('baseUrls');
+      dLog('baseUrl', key, value);
+      /** determine website from domain + path in url */
+      let
+      match = value.match(/([^?]+)/),
+      path = match && match[1],
+      /** using endsWith() ignores the protocol + port, which is e.g. http://  */
+      website = path && (
+        path.endsWith('crobiad.agwine.adelaide.edu.au/dawn/jbrowse/') ? 'Dawn' :
+        path.endsWith('wheat.pw.usda.gov/cgi-bin/GG3/report.cgi') ? 'GrainGenes' :
+        path.endsWith('plants.ensembl.org/Triticum_aestivum/Search/Results') ? 'Ensembl' : undefined
+      );
+
+      if (website === 'Dawn') {
+        /**  extract &loc=...&,  */
+        let
+          match = value.match(/(.*)\&loc=([^&]*)(.*)/),
+        /** e.g. "chr1B%3A321117692..321119204" */
+        loc = match && match[2];
+        /** - set axis y scale domain from loc (or set brush selection).  */
+        /** record the url without the &loc= parameter */
+        value = match && (match[1] + match[3]);
+      }
+      if (website) {
+        baseUrls[website] = value;
+        this.set('website', website);
+      }
+
+      return value;
+    }
+  }),
+
+  /*--------------------------------------------------------------------------*/
+
+
+  /** For each website, a function is defined which maps a featureName to a URL.
+   * Now will probably add functions to map location (brushedDomain) to a URL,
+   * so may factor the urlGrainGenes, urlEnsembl, urlDawn as classes with
+   * .urlForLocation and .urlForFeatureName.
+   */
   urlFunctions : {
     'GrainGenes' : 'urlGrainGenes',
-    'Ensembl' : 'urlEnsembl'
+    'Ensembl' : 'urlEnsembl',
+    'Dawn' : 'urlDawn'
   },
   /** Select function to calculate URL of featureName based on website.
    * @param this ember component
@@ -29,7 +87,7 @@ export default Ember.Component.extend({
     let
     urlFunctionName = this.get('urlFunctions')[website],
     urlFunction = urlFunctionName && this.get(urlFunctionName),
-    url = featureName && urlFunction(featureName);
+    url = featureName && urlFunction.apply(this, [featureName]);
     if (trace_url)
       console.log('urlOf', this.element, this.parentView.element, website, featureName, url);
     return url;
@@ -55,13 +113,58 @@ export default Ember.Component.extend({
       console.log('urlSelected', featureName, url);
     return url;
   }),
-  url : Ember.computed('website', 'featureName', function (newValue) {
+  urlLocation : Ember.computed('website', 'selectedBlock', 'brushedDomain', function (newValue) {
+    let url,
+    website = this.get('website'),
+    selectedBlock = this.get('selectedBlock');
+    dLog('urlLocation', selectedBlock);
+    if (selectedBlock) {
+      let
+        brushedDomain = this.get('brushedDomain'),
+      featureName = this.get('selectedFeatures0Name');
+       {
+         /* signature of urlOf function is different for urlLocation(). */
+         url = featureName && (website === 'Dawn') ?
+           this.urlDawn(featureName, selectedBlock, brushedDomain) : 
+           this.get('urlOf').apply(this, [website, featureName]);
+      }
+      if (trace_url)
+        console.log('urlLocation', featureName, url);
+    }
+    return url;
+  }),
+  url : Ember.computed('website', 'featureName', 'baseUrl', function (newValue) {
     let
       website = this.get('website'),
     featureName = this.get('featureName'),
     url = this.get('urlOf').apply(this, [website, featureName]);
     return url;
   }),
+  brush : Ember.computed('selectedBlock', function () {
+    let brush,
+    selectedBlock = this.get('selectedBlock');
+    dLog('selectedBlock', selectedBlock);
+    if (selectedBlock) {
+      brush = this.get('axisBrush').brushOfBlock(selectedBlock);
+    }
+    return brush;
+  }),
+  /**
+   * @return undefined if there is no brushed domain
+   */
+  brushedDomain : Ember.computed(
+    // selectedBlock will change when a new brush is started
+    'selectedBlock',
+    'brush.brushedDomain.0', 'brush.brushedDomain.1', 'brush.zoomCounter',
+    function () {
+      let brushedDomain,
+      brush = this.get('brush');
+      if (brush) {
+        brushedDomain = brush.get('brushedDomain');
+        dLog(brush, 'brushedDomain', brushedDomain);
+      }
+      return brushedDomain;
+    }),
 
   urlGrainGenes : function(featureName) {
     /** e.g. Xgwm382, Xgwm382.1-2A, Xgwm382-2A.1 */
@@ -78,7 +181,7 @@ export default Ember.Component.extend({
     options2=";show=possibleorthologs",
     url = protocol + domain + port + path + args1 + query + args2 + options2;
     if (trace_url)
-      console.log("urlEnsembl", featureName);
+      console.log("urlGrainGenes", featureName);
 
     return url;
   },
@@ -95,6 +198,40 @@ export default Ember.Component.extend({
     url = protocol + domain + port + path + args1 + query + args2;
     if (trace_url)
       console.log("urlEnsembl", featureName);
+
+    return url;
+  },
+
+  urlDawn : function(featureName, selectedBlock, brushedDomain) {
+    let
+      baseUrl = this.get('baseUrl'),
+    protocol = "http://",
+    domain = "crobiad.agwine.adelaide.edu.au",
+    port = "",
+    path = "/dawn/jbrowse/?",
+    args1='tracks=DNA%2CIWGSC_v1.0_HC_genes',
+    locationQuery;
+    dLog('urlDawn', selectedBlock);
+    if (selectedBlock) {
+      /** map to name used by Dawn, e.g. 'chr1A' */
+      let chrName = 'chr' + selectedBlock.get('name');
+      if (brushedDomain) {
+        let 
+          /** brush is Real, loc param is in base-pairs, so convert to Int. */
+          domainBp = brushedDomain && brushedDomain.map((d) => d.toFixed()),
+        locationText = domainBp[0] + '..' + domainBp[1];
+        locationQuery = 'loc=' + chrName + '%3A' + locationText;
+        this.set('anchorLocationText', locationText);
+      }
+    }
+    let
+    // query="q=" + featureName + ";",
+    args2='highlight=',
+    url = baseUrl || 
+      (protocol + domain + port + path) + [args1, args2].join('&');
+    url += '&' + locationQuery;
+    if (trace_url)
+      console.log("urlDawn", featureName);
 
     return url;
   }
