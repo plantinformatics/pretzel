6547ac710ae14a245c85f8f332e60170f04c8a96
Don <Don.Isdale@gmail.com>
2020-04-29 13:17:09 +1000

    By default, don't display the data-sources list
    api-servers.js : add showList.
    api-servers.hbs : wrap ul.list-group / each servers with #if showList, and else show addNewDatasource button in panel-heading
    frontend/app/ :
     c9231d5  966 Apr 29 12:26  components/form/api-servers.js
     a4886ce  931 Apr 29 12:26  templates/components/form/api-servers.hbs

--- frontend/app/components/form/api-servers.js
+++ frontend/app/components/form/api-servers.js
@@ -2,8 +2,24 @@ import Ember from 'ember';
 
 const { inject: { service }, Component } = Ember;
 
+/* global $ */
+
+/*----------------------------------------------------------------------------*/
+
+/** Enable display of form/api-server for each data source / api-server.
+ * This was useful when developing the connection setup.
+ * It displays the token, which is useful for command-line upload; otherwise
+ * users have to go hunting through web inspector : Application : Storage :
+ * cookies : ember_simple_auth-session : token.
+ */
+const showList = false;
+
+/*----------------------------------------------------------------------------*/
+
 export default Component.extend({
   apiServers: service(),
+
+  showList,
 
   /* Early prototypes, up until commit fa0c40e, had action&function addEmpty(),
    * but not needed so dropped. */
