be584c12eddc34c47b7389c10da4665c09a3fddf
Don <Don.Isdale@gmail.com>
2021-02-01 19:27:18 +1100

    editing of dataset meta json : re-enable editing, fix read-only.
    manage-dataset.js : add jsoneditorOptions, with onEditable returning false.
    datasetMeta() : copy to .currentMeta, because json-editor modifies its json param
    mutateJson() : no need to modify .currentMeta.
    app.scss : override -webkit-user-modify from ember-content-editable, for .jsoneditor-{field,value}[contenteditable=false];  this is required to make the field read-only in Chrome.
    manage-dataset.hbs : json-editor args : pass either options=jsoneditorOptions for view, or json=currentMeta for edit.     ember-jsoneditor has an options CP, so setting options gets a warning; will try ember-cli-jsoneditor instead, which does have disabled= param
    part of #216 : Metadata : Issue #222 - Editing no long works
    frontend/app/ :
     81836d3  1659 Feb  1 17:37  components/panel/manage-dataset.js
     a417c11 38175 Feb  1 19:11  styles/app.scss
     e84603a  2056 Feb  1 18:44  templates/components/panel/manage-dataset.hbs

--- frontend/app/components/panel/manage-dataset.js
+++ frontend/app/components/panel/manage-dataset.js
@@ -11,12 +11,21 @@ export default ManageBase.extend({
   editorVisible: false,
   currentMeta: {},
 
+  jsoneditorOptions : {
+    onEditable : function() { dLog('onEditable'); return false; }
+  },
+
   ownedByMe: alias("dataset.owner"),
   datasetMeta: computed("dataset._meta", function() {
     let meta = this.get("dataset._meta") || {},
     apiHost = this.get("dataset.store.name");
     if (apiHost)
       meta.apiHost = apiHost;
+    /** Copy to .currentMeta because :
+     * ember-jsoneditor/addon/components/json-editor.js:  _change() does this.set('json', json);
+     * so we can't pass {{json-editor json=datasetMeta ... }}
+     */
+    this.set('currentMeta', Object.assign({}, meta));
     return meta;
   }),
 
@@ -25,9 +34,12 @@ export default ManageBase.extend({
       this.toggleProperty('editorVisible');
     },
     mutateJson(json) {
+      /*  mutateJson() is called after _change() has changed the json value
+       *  passed (.currentMeta), so no need to change currentMeta here.
       if (trace > 1)
         dLog('mutateJson()', 'currentMeta => ', this.get("currentMeta"));
       this.set("currentMeta", json);
+      */
       if (trace > 1)
         dLog('mutateJson()', 'currentMeta => ', this.get("currentMeta"));
       // this.get("dataset").save()
