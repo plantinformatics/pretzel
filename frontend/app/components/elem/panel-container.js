5fc10d187e85f199c10bd8b0cd4080072fbb6ec3
Don <Don.Isdale@gmail.com>
2021-02-23 20:57:51 +1100

    show a display toggle for all panel-container, in panel-heading.
    panel-container.js : add showComponent, toggleShow().
    panel-heading.js : add comment.
    app.scss : add .panel.no-side-border for #left-panel-upload.
    
    panel-heading.hbs : move x-toggle here from api-servers.hbs and manage-view.hbs.
    api-servers.hbs : pass showComponent=showList to panel-container, and panelContainer to panel-heading.  Use showComponent in place of showList.
    
    For each of these files, pass panelContainer to panel-heading, and wrap content elements with #if showComponent :
      manage-block.hbs : for both panel-container -s
      manage-explorer.hbs
      manage-search.hbs
      manage-view.hbs : also, for the 2nd panel-container, pass showComponent=showList to panel-container,  use showComponent in place of show.
      paths-table.hbs
      upload-data.hbs : also pass class=no-side-border
      view-controls.hbs
    
    #216 : https://github.com/plantinformatics/pretzel/issues/216#issuecomment-773060872
    frontend/app/ :
     8dc9261  1078 Feb 23 19:56  components/elem/panel-container.js
     bf34c86   707 Feb 23 19:47  components/elem/panel-heading.js
     214fbbb 39937 Feb 23 19:51  styles/app.scss
    frontend/app/templates/components/ :
     28c0dbe    15 Feb 23 16:02  elem/panel-container.hbs
     a360926   265 Feb 23 20:01  elem/panel-heading.hbs
     507e1fa  1034 Feb 23 19:15  form/api-servers.hbs
     ccbc62a  4353 Feb 23 16:43  panel/manage-block.hbs
     c391b76  8298 Feb 23 16:43  panel/manage-explorer.hbs
     be46eeb   878 Feb 23 16:43  panel/manage-search.hbs
     edd73d4  3510 Feb 23 19:02  panel/manage-view.hbs
     b6cd019  3199 Feb 23 16:43  panel/paths-table.hbs
     cda2b8f  1641 Feb 23 19:49  panel/upload-data.hbs
     0d0f00e  7320 Feb 23 16:15  panel/view-controls.hbs

--- frontend/app/components/elem/panel-container.js
+++ frontend/app/components/elem/panel-container.js
@@ -1,6 +1,18 @@
 import { computed } from '@ember/object';
 import Component from '@ember/component';
 
+const dLog = console.debug;
+
+/** Used to group elements in the left and right side panels.
+ * Owns the flag and action which enables display of the child
+ * components after the heading.
+ * The first child is expected to be panel-heading, which provides a
+ * toggle button for showComponent / toggleShow.
+ *
+ * @param showComponent optional, default true : if given, provides
+ * the initial value of showComponent.  If true, the child elements
+ * and components are displayed.
+ */
 export default Component.extend({
   // attributes
   // classes
@@ -8,5 +20,13 @@ export default Component.extend({
   panelClass: computed('state', function() {
     return 'panel panel-' + this.state
   }),
+  showComponent : true,
+  /** later can make this @action instead of passing panelContainer as action param. */
+  // @action
+  toggleShow(panelContainer) {
+    // this is currently panel-heading
+    dLog('toggleShow', panelContainer.showComponent);
+    panelContainer.toggleProperty('showComponent');
+  }
   // actions
 });
