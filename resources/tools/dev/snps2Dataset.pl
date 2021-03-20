430342de87fa0dd3e072b8228aaf280f73bbb3db
Don <Don.Isdale@gmail.com>
2021-03-20 14:14:45 +1100

    add GM support in SNP csv->json, add tsv support, enable configuration of column order, handle variations of format - trim outside quotes and spaces.
    snps2Dataset.pl :
    add $columnsKeyString, $columnsKeyPrefixed
    factor to form headerLine(), with added check on $lineNumber ($.).
    (can add options for : the regexp in headerLine(), and $columnsKeyString)
    Add $namespace and $commonName - options -n and -c.
    These 3 added configs can be options : factor to form $fieldSeparator, and add comma.  Add $addValues, $isGM, $datasetHeaderGM.
    Add trimOutsideQuotesAndSpaces() - applied to c_chr and c_name.
    printFeature() : use c_ enums to address $a and splice() to remove them, replacing shift() and handling variations in column order.
    Round $pos to 8 decimal places, because ssconvert does not round.
     80500f9  9899 Mar 19 19:45 resources/tools/dev/snps2Dataset.pl

--- resources/tools/dev/snps2Dataset.pl
+++ resources/tools/dev/snps2Dataset.pl
@@ -24,12 +24,42 @@ use Getopt::Std;	# for getopt()
 
 #-------------------------------------------------------------------------------
 
+my $columnsKeyString;
+
 # Define enum-like constants for the indexes of the input columns.
 # Not fully used, c_char is used in snpLine(), but printFeature() unpacks the input fields directly using shift @a.
 #   based on: https://stackoverflow.com/a/25512227, kbro
 # Prefix some of the enum names with c_ (column) to avoid namespace clash with e.g. perl chr().
 # data example : chr1A	22298	scaffold38755_22298	T/C
-use constant ColumnsEnum => qw(c_chr c_pos c_scaffold_pos c_ref_alt);
+# scaffold_pos -> name
+$columnsKeyString = "chr pos name ref_alt";
+
+#SNP_20002403,LG7.2,40.5
+#PBA_LC_0373,LG7.3,0
+#SSR184,LG7.3,1.9
+#SNP_20004741,LG7.3,7.2
+# $columnsKeyString = "name chr pos";
+# This may be a requirement :
+# my $chrPrefix = 'L.';
+# Assumption : if chr has 2 '.' after $chrPrefix then scope is : trim off the 2nd . and following chars.
+#Lc_ILL_00694,L.5.1,480.1670411
+#Lc_ILL_00714,L.5.2,0
+#Lc_ILL_00037,L.5.2,4.321070321
+
+
+# equivalent to e.g : qw(c_chr c_pos c_name c_ref_alt)
+# /r for non-destructive, allows chaining.
+my $columnsKeyPrefixed;
+BEGIN
+{
+    $columnsKeyString = "name chr pos";
+    $columnsKeyString = "chr name pos";
+    $columnsKeyPrefixed = $columnsKeyString
+    =~ s/,/ /rg
+    =~ s/^/c_/r
+    =~ s/ / c_/rg;
+}
+use constant ColumnsEnum => split(' ', $columnsKeyPrefixed);
 BEGIN
 {
     eval "use constant (ColumnsEnum)[$_] => $_;" foreach 0..(ColumnsEnum)-1;
@@ -40,7 +70,13 @@ sub convertInput();
 sub createDataset();
 sub appendToBlock();
 
-
+# @return true if the given line is a column header row
+sub headerLine($$) {
+    my ($line, $lineNumber) = @_;
+    my $isHeader = ($line =~ m/^label	chr	pos/)
+	|| (($lineNumber == 1) && ($line =~ m/Marker.*Chromosome/i));
+    return $isHeader;
+}
 #-------------------------------------------------------------------------------
 
 my $shortName = "WGS";	# option, default : Exome
@@ -93,20 +129,42 @@ EOF
 
 ## Get options from ARGV
 my %options;
-getopts("vhd:p:b:", \%options);
+getopts("vhd:p:b:n:c:", \%options);
 
 ## Version and help options display
 use constant versionMsg => "2020 Dec 07 (Don Isdale).\n";
 use constant usageMsg => <<EOF;
 	Usage e.g. : $0 [-d Exome_SNPs_1A -p Triticum_aestivum_IWGSC_RefSeq_v1.0 ] _or_ -b blockId  < IWGSC_RefSeq_v1.0.EXOME_SNPs.chr1A.tsv > Exome_SNPs_1A.json
+	Optional params : -n namespace [empty | 90k | ... ]  -c "common name"
 EOF
 
 my $datasetName = $options{d};
 my $parentName = $options{p};
 my $blockId = $options{b};
+my $namespace = $options{n};
+my $commonName = $options{c};
+
+my $fieldSeparator = ',';	# '\t'
 
 
 my $refAltSlash = 0;	# option, default 0
+my $addValues = 0;	# option : add values : { other columns, }
+# option : if  $namespace =~ m/90k/ etc,  use  $datasetHeaderGM
+my $isGM = 1;
+
+#-------------------------------------------------------------------------------
+
+my $datasetHeaderGM = <<EOF;
+{
+    "name": "myMap",
+    "namespace" : "$namespace",
+    "meta" : { "type" : "Genetic Map", "commonName" : "$commonName" },
+    "blocks": [
+EOF
+
+#-------------------------------------------------------------------------------
+
+
 
 if ($options{v}) {
     print versionMsg;
@@ -149,6 +207,9 @@ my $blockSeparator;
 
 sub createDataset()
 {
+    if ($isGM) {
+	$datasetHeader = $datasetHeaderGM;
+    }
 
     $datasetHeader =~ s/myMap/$datasetName/;
     $datasetHeader =~ s/Triticum_aestivum_IWGSC_RefSeq_v1.0/$parentName/g;
@@ -178,7 +239,7 @@ sub convertInput()
         chomp;
         # commenting out this condition will output the column headers in the JSON,
         # which is a useful check of column alignment with the ColumnsEnum.
-        if (! m/^label	chr	pos/)
+        if (! headerLine($_, $.))
         { snpLine($_); }
     }
 }
@@ -194,12 +255,15 @@ sub snpLine($)
 {
     my ($line) = @_;
     # input line e.g.
-    #c_chr c_pos c_scaffold_pos c_ref_alt
+    #c_chr c_pos c_name c_ref_alt
     #chr1A	22298	scaffold38755_22298	T/C
 
-    my @a =  split( '\t', $line);
+    my @a =  split($fieldSeparator, $line);
     # tsv datasets often follow the naming convention 'chr1A';  Pretzel data omits 'chr' for block scope & name : '1A'.
     $a[c_chr] =~ s/^chr//;
+    $a[c_chr] = trimOutsideQuotesAndSpaces($a[c_chr]);
+    $a[c_name] = trimOutsideQuotesAndSpaces($a[c_name]);
+
     my $c = $a[c_chr];
     if (! defined($lastChr) || ($lastChr ne $c))
     {
@@ -230,6 +294,22 @@ sub snpLine($)
     printFeature(@a);
 }
 
+# Strip off outside " and spaces, to handle e.g.
+#   "LG4 ",Ca_2289,0
+#   Ps_ILL_03447,"LG 2",0
+sub trimOutsideQuotesAndSpaces($) {
+    my ($label) = @_;
+    if ($label =~ m/"/) {
+	$label =~ s/^"//;
+	$label =~ s/"$//;
+    }
+    if ($label =~ m/ /) {
+	$label =~ s/^ //;
+	$label =~ s/ $//;
+    }
+    return $label;
+}
+
 # For printing array as comma-separated list.
 # Could make this local if it clashed with any other print.
 # As an alternative to using join to construct $aCsv in printFeature(), can do :
@@ -241,17 +321,38 @@ sub printFeature($)
 {
     my (@a) = @_;
 
-    my $chr = shift @a;
-    my $pos = shift @a;
-    my $label = shift @a;	# c_scaffold_pos
+    my $chr = $a[c_chr];
+    my $pos = $a[c_pos];
+    my $label = $a[c_name];
+    splice(@a, c_chr, 1);
+    splice(@a, c_pos, 1);
+    splice(@a, c_name, 1);
+
+    # Recognise decimal fraction aliasing and round the number. 
+    # ssconvert apparently has different rounding to libreoffice, as the former
+    # expresses some decimal fractions with recurring 0 or 9.
+    # e.g comparing output from libreoffice and ssconvert respectively 
+    #   < SNP_40002085,LG1,1.3
+    #   > SNP_40002085,LG1,1.2999999999999998
+    #   < SNP_40001996,LG1,7.6
+    #   > SNP_40001996,LG1,7.6000000000000005
+    # ssconvert handles multiple work-sheets within the .xslx, but libreoffice does not.
+    # If the number has a few decimal digits in the source spreadsheet, then
+    # the number of 0-s or 9-s to match here may be as few as 11. match a minimum of 6.
+    # The SNP / marker name may also contain 4 0-s, but that is a different column and they are unlikely to have 8.
+    if ($pos =~ m/000000|999999/) {
+	$pos = (sprintf('%.8f', $pos) =~ s/0+$//r =~ s/\.$//r);
+    }
+
     my $values = "";
     my $ref_alt;
     if ($refAltSlash)
     { $ref_alt = "\"ref\" : \"" . (shift @a) . "\""; }
-    else
+    elsif ($addValues)
     { $ref_alt = "\"ref\" : \"$a[0]\"" . ", " . "\"alt\": \"$a[1]\""; };
     my $indent = "                    ";
     if ($ref_alt) { $values .=  ",\n" . $indent . "\"values\" : {" . $ref_alt . "}"; }
+
     print <<EOF;
                {
                     "name": "$label",
