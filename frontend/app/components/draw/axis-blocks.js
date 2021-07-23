98027e60d6c94b730ec0dfe0c3d2f293f28a1f2b
Don <Don.Isdale@gmail.com>
2021-07-23 20:29:32 +1000

    re-connect rightEdge position update
    axis-blocks.js : roll back to previous version (b220b866), before 5c215cb1 which factored component-layout out of axis-blocks so that it could implement centre & right in axis-blocks;  there is a problem with the dependencies through that layer of indirection, so instead of solving that atm, this commit rolls back this part of the change, because as yet we don't have any fixed width tracks in the right region.   This means that the functions moved to utils/draw/component-layout.js are now duplicates, and are not used.
    axis-2d.hbs : to match that, append .centre in passing childWidths into axis-blocks

--- frontend/app/components/draw/axis-blocks.js
+++ frontend/app/components/draw/axis-blocks.js
@@ -1,16 +1,15 @@
-import EmberObject, { computed } from '@ember/object';
+import { next } from '@ember/runloop';
+import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-
-import componentLayout from '../../utils/draw/component-layout';
 
 const dLog = console.debug;
 
 /*----------------------------------------------------------------------------*/
 
-/** QTL track width plus spacing  */
-const blockWidth = 15; 
+/** indexes into the result of allocatedWidthBlocks. */
+const Index_block = 0, Index_startOffset = 1, Index_endOffset = 2;
 
 /*----------------------------------------------------------------------------*/
 
@@ -32,33 +31,59 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
-
-    this.set('centre', componentLayout.create({
-      blocks : this.get('blocks'),
-      trackWidth : this.get('trackWidth'),
-      className : this.get('className'),
-      childWidths : this.get('childWidths.centre')
-    }));
-    /** this may be used if there is a call for fixed-width tracks for QTLs, otherwise not needed.  */
-    this.set('right', componentLayout.create(
-      {
-        blocks : this.get('blocks'),
-        trackWidth : 15,
-        className : 'qtl',
-        childWidths : this.get('childWidths.right')
-      }));
   },
 
 
   blocks : alias('dataBlocks'),
 
-  centre : undefined,
-  right : undefined,
+  /** Allocate fixed-width horizontal space for each block.
+   * @return [[block, startOffset, endOffset], ...],
+   */
+  allocatedWidthBlocks : computed('blocks.[]', function () {
+    let
+    blocks = this.get('blocks'),
+    /** trackWidth is actually the <rect> width for tracks.  Add trackWidth (10px) for spacing */
+    trackWidth = this.get('trackWidth') * 3,
+    aw = blocks.map((block, i) => [
+      block,
+      i * trackWidth,
+      (i+1) * trackWidth]);
 
-  allocatedWidthBlocks : alias('centre.allocatedWidthBlocks'),
-  allocatedWidth : alias('centre.allocatedWidth'),
-  widthsById : alias('centre.widthsById'),
-  allocatedWidthForBlock(blockId) { return this.centre.allocatedWidthForBlock(blockId); },
+    let width = aw.length ? aw[aw.length-1][Index_endOffset] : 0;
+    dLog('allocatedWidthBlocks', aw, width);
+    let childWidths = this.get('childWidths');
+    // [min, max] width
+    next(() => childWidths.set(this.get('className'), [width, width]));
+
+    return aw;
+  }),
+  /** as for allocatedWidthBlocks, without the block.
+   * @return [[startOffset, endOffset], ...],   parallel to this.blocks[]
+   */
+  allocatedWidth : computed('allocatedWidthBlocks.[]', function () {
+    let aw = this.get('allocatedWidthBlocks').map((bi) => bi.slice(Index_startOffset));
+    return aw;
+  }),
+  /**
+   * @return symbolic array (hash) which maps from blockId to allocatedWidth for that block
+   * [blockId] -> array of [startOffset, width].
+   */
+  widthsById: computed(
+    'allocatedWidthBlocks.[]',
+    function() {
+      let byId = this.get('allocatedWidthBlocks').reduce((r, b) => {
+        r[b[Index_block].get('id')] = b.slice(Index_startOffset); return r;
+      }, {});
+      return byId;
+    }),
+  /** Lookup  */
+  allocatedWidthForBlock(blockId) {
+    let
+    /** [blockId] -> array of [startOffset, width]. */
+    blocksWidths = this.get('widthsById'),
+    aw = blocksWidths[blockId];
+    return aw;
+  },
 
   /*--------------------------------------------------------------------------*/
 
