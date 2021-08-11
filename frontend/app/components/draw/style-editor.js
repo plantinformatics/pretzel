da4a14f2489d2506361869ad01447a846a70c835
Don <Don.Isdale@gmail.com>
2021-08-11 16:37:57 +1000

    style editor : colour multiple elements; select fill/stroke
    style-editor.js :
    factor to form : getSelectedElements(), showStyleEditor() (same in synteny-blocks.js; these will move to a service).
    add enableSet{Fill,Stroke}, which selectively enable setting {fill,stroke}.
    
    synteny-blocks.js :
    configureSyntenyBlockClicks() : call syntenyBlockAdjust() if ctrlKey
    add syntenyBlockAdjust()
    
    style-editor.hbs :  add input checkbox enableSet{Fill,Stroke}

--- frontend/app/components/draw/style-editor.js
+++ frontend/app/components/draw/style-editor.js
@@ -8,22 +8,36 @@ import SpectrumColorPickerComponent from 'ember-spectrum-color-picker/components
 
 import { stacks } from '../../utils/stacks';
 
+/*----------------------------------------------------------------------------*/
 
 const dLog = console.debug;
 
+/*----------------------------------------------------------------------------*/
+
+function getSelectedElements() {
+  return stacks?.oa?.selectedElements;
+}
+function showStyleEditor(show) {
+    stacks?.oa?.eventBus && stacks.oa.eventBus.set('showStyleEditor', show);
+}
+
+/*----------------------------------------------------------------------------*/
 
 export default Component.extend({
 
+  enableSetFill : true,
+  enableSetStroke : true,
+
   elementColour: computed({
     get() {
-      let selectedElements = stacks?.oa?.selectedElements;
+      let selectedElements = getSelectedElements();
       let elements = selectedElements;
       let colour = elements.length && window.getComputedStyle(elements[0])['stroke'];
       return colour;
     },
 
     set(key, colour) {
-      let selectedElements = stacks?.oa?.selectedElements;
+      let selectedElements = getSelectedElements();
       let elements = selectedElements;
       dLog('set', key, colour, elements.length);
       /* use .style instead of .attr to apply fill/stroke so that they
@@ -31,11 +45,16 @@ export default Component.extend({
        * refn https://www.w3.org/TR/SVG/styling.html#PresentationAttributes
        * https://stackoverflow.com/questions/47088409/svg-attributes-beaten-by-cssstyle-in-priority/47088443#47088443
        */
-      d3.selectAll(elements)
-        .style('fill', colour)
+      let elementsS = d3.selectAll(elements);
+      if (this.get('enableSetFill')) {
+        elementsS
+        .style('fill', colour);
+      }
+      if (this.get('enableSetStroke')) {
+        elementsS
         .style('stroke', colour);
-
-      stacks?.oa?.eventBus && stacks.oa.eventBus.set('showStyleEditor', false);
+      }
+      showStyleEditor(false);
 
       return colour;
     }
--- frontend/app/components/draw/synteny-blocks.js
+++ frontend/app/components/draw/synteny-blocks.js
@@ -14,10 +14,19 @@ function configureSyntenyBlockClicks(selection) {
     .on('click', function (d, i, g) {
       if (d3.event.shiftKey) {
         syntenyBlockSelect.apply(this, [d, i, g]);
+      } else if (d3.event.ctrlKey) {
+        syntenyBlockAdjust.apply(this, [d, i, g]);
       } else {
         syntenyBlockEdit.apply(this, [d, i, g]);
       };
     });
+}
+
+function getSelectedElements() {
+  return stacks?.oa?.selectedElements;
+}
+function showStyleEditor(show) {
+    stacks?.oa?.eventBus && stacks.oa.eventBus.set('showStyleEditor', show);
 }
 
 /** Store this element in selectedElements[], the array of elements
@@ -29,19 +38,32 @@ function configureSyntenyBlockClicks(selection) {
  */
 function syntenyBlockEdit(d, i, g) {
   /** not decided yet whether selectedElements will be 1 per draw-map or singleton. */
-  let selectedElements = stacks?.oa?.selectedElements;
+  let selectedElements = getSelectedElements();
   if (selectedElements && Array.isArray(selectedElements)) {
     selectedElements.splice(0);
     selectedElements.push(this);
-    stacks?.oa?.eventBus && stacks.oa.eventBus.set('showStyleEditor', true);
+    showStyleEditor(true);
   }
 }
+/** Add this element to selectedElements, if it is not already present.
+ */
 function syntenyBlockSelect(d, i, g) {
-  let selectedElements = stacks?.oa?.selectedElements;
+  let selectedElements = getSelectedElements();
   if (selectedElements) {
-    selectedElements.push(this);
+    selectedElements.addObject(this);
   }
 }
+/** Add this element to selectedElements, if it is not already present.
+ * Show the style-editor
+ */
+function syntenyBlockAdjust(d, i, g) {
+  let selectedElements = getSelectedElements();
+  if (selectedElements) {
+    selectedElements.addObject(this);
+    showStyleEditor(true);
+  }
+}
+
 
 /*----------------------------------------------------------------------------*/
 
