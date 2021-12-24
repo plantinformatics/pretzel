84b7a5084bc2141912de1760563f79039e8baef8
Don <Don.Isdale@gmail.com>
2021-12-24 11:37:34 +1100

    explorer Ontology tree : set visibility of Ontology branch which contains the clicked block +
    entry-block-add-button.js : add setOntologyVisible();  instead of walking up to check for parent entry- which is Ontology leaf (trait) node, this could be configured via a context param passed down through each entry- component (or meta value as done for entry-expander checkbox).

--- frontend/app/components/record/entry-block-add-button.js
+++ frontend/app/components/record/entry-block-add-button.js
@@ -1,7 +1,12 @@
+import { inject as service } from '@ember/service';
+
 import EntryBase from './entry-base';
 
+import { ontologyIdFromIdText } from '../../utils/value-tree';
 
 export default EntryBase.extend({
+  ontology : service('data/ontology'),
+
   tagName: '',
 
 
@@ -10,9 +15,30 @@ export default EntryBase.extend({
     loadBlock(block) {
       console.log('entry-block-add-button: loadBlock', block, arguments);
       this.sendAction('loadBlock', block);
+      this.setOntologyVisible();
     }
-  } // actions
+  }, // actions
   /*--------------------------------------------------------------------------*/
 
+  /** If this component is in explorer ontology tree, set as visible the
+   * OntologyId which this component is within.
+   */
+  setOntologyVisible() {
+    /** record/entry-* component */
+    let entry = this, isTrait;
+    while (
+      entry.parentView &&
+        !((isTrait = entry?.values && (entry?.levelMeta?.get(entry?.values) == 'trait')))    ) {
+      // dLog(entry._debugContainerKey, entry?.values);
+      entry = entry.parentView;
+    }
+    if (isTrait) {
+      let
+      values = entry.values,
+      ontologyId = values.id ||
+      (values.name && ontologyIdFromIdText(values.name));
+      this.get('ontology').setOntologyIsVisible(ontologyId, true);
+    }
+  },
 
 });
