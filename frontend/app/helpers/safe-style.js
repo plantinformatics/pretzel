29516dbd1cda75bdc7cf1f2e410d71b54bf6cf9d
Don <Don.Isdale@gmail.com>
2022-01-31 19:50:08 +1100

    use colour border instead of solid block in view Ontology tree
    safe-style.js : safeStyle() : use namedParams instead of positionalParams, so that different / multiple css attributes can be set. Drop use of CSS.escape() - not required; the case which slices # from colorHex works but is not reqired.
    app.scss : add .wide-border
    trait-qtl.hbs : insert background-color=  to use of safe-style.
    entry-expander.hbs and entry-leaf-ontology.hbs : insert border-color= to use of safe-style;  add class .wide-border

--- frontend/app/helpers/safe-style.js
+++ frontend/app/helpers/safe-style.js
@@ -4,16 +4,25 @@ import { htmlSafe } from '@ember/template';
 /* global CSS */
 
 /** Sanitize a colour value for binding to style=background-color:...
- * @params  currently just [colour], but can be [ [field-name, value], ... ]
+ * @param positionalParams  not used (positionalParams[0] was background-color until f49adc5a)
+ * @param namedParams e.g. {background-color : "#1f77b4", ... }
  */
-export default helper(function safeStyle(params/*, hash*/) {
-  let result;
-  if (params && params[0] && params[0]?.startsWith('#')) {
-    /** param value e.g. "#1f77b4"
-     * split off the # because CSS.escape() will map that to \#, which the browser will reject. */
-    let colorHex = params[0].slice(1);
-    let color = CSS.escape(colorHex);
-    result = htmlSafe("background-color: #" + color);
-  }
+export default helper(function safeStyle(positionalParams, namedParams) {
+  let
+  texts = Object.entries(namedParams).map(([name, value]) => {
+    let paramText;
+    if (value === undefined) {
+    } else if (false && name.endsWith('-color') && value.startsWith('#')) {
+      /** param value e.g. "#1f77b4"
+       * split off the # because CSS.escape() will map that to \#, which the browser will reject. */
+      let colorHex = value.slice(1);
+      let color = CSS.escape(colorHex);
+      paramText = htmlSafe(name + ": #" + color);
+    } else {
+      paramText = htmlSafe(name + ":" + value);
+    }
+    return paramText;
+  }),
+  result = texts.join('; ');
   return result;
 });
