f9dbfccd79481e55ff6322edbbf4bd01b2b86ba6
Don <Don.Isdale@gmail.com>
2022-03-18 20:43:45 +1100

    add helper to-array-promise-proxy
    this should have been included in preceding commit 7bb41933

---
+++ frontend/tests/integration/helpers/to-array-promise-proxy-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | to-array-promise-proxy', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it renders', async function(assert) {
+    this.set('inputValue', '1234');
+
+    await render(hbs`{{to-array-promise-proxy inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), '1234');
+  });
+});
