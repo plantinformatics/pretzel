faa8d8a86911d1d266eb0032772f7326fd4b8fd8
Don <Don.Isdale@gmail.com>
2022-03-30 16:54:07 +1100

    Improve on delay before collating ClientGroups.
    This performs well on local;  timing on development server is different so this commit will be tested there.
    client-groups.js : init() and whenModels() : trialled event listeners for : app, db, topology (t); no usable result - these can be dropped.
    It was found that checking either ClientGroup.s.db or topology in poll is sufficient precursor to calling to ClientGroup.aggregate().

--- lb4app/lb3app/common/utilities/client-groups.js
+++ lb4app/lb3app/common/utilities/client-groups.js
@@ -11,6 +11,29 @@ class ClientGroups {
   }
   init(app) {
     whenModels(app, (ClientGroup) => this.update(ClientGroup));
+
+    /** These event listeners don't fire; app does define these event names :
+     * app.eventNames()
+     * (8) ['mount', 'modelRemoted', 'stateChanged', 'loaded', 'started', 'modelDeleted', 'remoteMethodAdded', 'remoteMethodDisabled']
+     */
+    if (app) {
+      app.on('stateChanged', data => {
+        console.log('stateChanged', data);
+      });
+
+      app.once('mount', function () {
+        console.log('app.once(mount)', app);
+      });
+
+      app.once('loaded', function () {
+        console.log('app.once(loaded)', app);
+      });
+      app.once('started', function () {
+        console.log('app.once(started)', app);
+      });
+
+    }
+
   }
 };
 
@@ -18,36 +41,78 @@ exports.clientGroups = new ClientGroups();
 
 // -----------------------------------------------------------------------------
 
-/** Call fn with models.ClientGroup when ClientGroup.aggregate is defined
+/** Call fn with models.ClientGroup when it is OK to call ClientGroup.aggregate.
+ *
+ * The purpose of this delay is to avoid this error :
+ *   loopback-connector-mongodb/lib/mongodb.js:384
+ *     if (!this.db) {
+ *       throw new Error(g.f('{{MongoDB}} connection is not established'));
  */
 function whenModels(app, fn) {
+  /** After experimentation and reading the source (haven't found a doc covering this),
+   * it seems that when topology is defined, it is OK to call ClientGroup.aggregate.
+   * The function which throws the error, MongoDB.prototype.collection(),
+   * specifically tests this.db which seems to be e.g. ClientGroup.s.db.
+   * Polling until this value is defined has proven sufficient.
+   *
+   * None of the event listeners fired, apart from topology : 'connectionReady'
+   * which fires after the .aggregate() call starts - perhaps a lazy connection.
+   */
   let 
   d, db, ds, ClientGroup,
+  c, t, updateStarted,
   ok = (
     (d = /*models.ClientGroup.*/ app.datasources) &&
       // m = d.mongoDs.connector._models,
     (db = d.db) && db.connected && db.initialized &&
     (ds = d.db?.models?.ClientGroup?.getDataSource()) &&
+    (c = ds.connector) && 
+    (t = c.client?.topology) &&
     (ClientGroup = ds.connector?.collection('ClientGroup')) &&
     (ClientGroup.aggregate)
     );
-  if (ok) {
-    /** Alternative to polling is these event listeners; not working yet.  */
+  console.log('whenModels', !!d, !!db, !!ds, !!c, !!t, !!ClientGroup?.db);
+  if (ClientGroup?.s?.db) {
+    updateStarted = true;
+    fn(ClientGroup);
+  }
+  if (t) {
+    console.log('ds', /*d, db, 'ds', ds*/);
+    /** Alternative to polling is these event listeners; not working yet.
+     * ds.eventNames()  []
+     * db.eventNames()  []
+     * c.client.eventNames()  ['newListener']
+     */
     db.on('error', function (err) {
       console.log('Error : Failed to connect to database');
     });
 
     // mongoose : open
-    db.once('serverOpening', function () {
+    db.once('connect', function () {
       console.log("Info : Connected to database");
     });
-    /* Some additional time is required after ok.  5sec is sufficient.
-     * otherwise : loopback-connector-mongodb/lib/mongodb.js:384
-     * throw new Error(g.f('{{MongoDB}} connection is not established'));
-     */
-    setTimeout(() => fn(ClientGroup), 5000, 'ClientGroup update');
 
-  } else {
+    /** t.eventNames() does include these event names; only connectionReady fires.
+     * (35) ['authenticated', 'error', 'timeout', 'close', 'parseError', 'fullsetup', 'all', 'reconnect', 'commandStarted', 'commandSucceeded', 'commandFailed', 'serverOpening', 'serverClosed', 'serverDescriptionChanged', 'serverHeartbeatStarted', 'serverHeartbeatSucceeded', 'serverHeartbeatFailed', 'topologyOpening', 'topologyClosed', 'topologyDescriptionChanged', 'joined', 'left', 'ping', 'ha', 'connectionPoolCreated', 'connectionPoolClosed', 'connectionCreated', 'connectionReady', 'connectionClosed', 'connectionCheckOutStarted', 'connectionCheckOutFailed', 'connectionCheckedOut', 'connectionCheckedIn', 'connectionPoolCleared', 'open']
+     */
+    t.once('serverOpening', function () {
+      console.log("Info : database : topology : serverOpening");
+    });
+    t.once('topologyOpening', function () {
+      console.log("Info : database : topology : topologyOpening");
+    });
+    t.once('all', function (a, b, c) {
+      console.log("Info : database : topology : all", a, b, c);
+    });    
+    t.once('connectionReady', function () {
+      console.log("Info : database : topology : connectionReady", !!ClientGroup?.s?.db);
+    });
+
+    db.once('connected', function () {
+      console.log("Info : database : connected");
+    });
+  }
+  if (! updateStarted) {
     setTimeout(() => whenModels(app, fn), 1000, 'ClientGroup update');
   }
   return ok;
