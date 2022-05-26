fd9909c2a43dc9df55d49ea232206091278649b3
Don <Don.Isdale@gmail.com>
2022-05-26 21:09:17 +1000

    QTL spreadsheet upload : don't set .value_0 to []
     ... as was occurring when .value is [] because position was defined by .values.flankingMarkers.
    upload.js : insert_features_recursive() :
    if feature.value_0 is null then use that value; also set it to null if undefined and value is [] (previously it was set to value i.e. [] in this case).

--- lb4app/lb3app/common/utilities/upload.js
+++ lb4app/lb3app/common/utilities/upload.js
@@ -196,8 +196,15 @@ function insert_features_recursive(db, dataset_id, features_to_insert, ordered, 
       });
       delete feature.features;
     }
-    if (! notDefined(feature.value) && notDefined(feature.value_0)) {
-      feature.value_0 = feature.value.length ? feature.value[0] : feature.value;
+    /** snps2Dataset.pl generates value_0:null for QTL;
+     * models/block.js : loadRequiredData() -> allFeatures() ->
+     * getBlockFeaturesInterval( true) loads all the features of a QTL block, so
+     * QTL value_0 is not used.
+     */
+    if (! notDefined(feature.value) && (feature.value_0 === undefined)) {
+      const value = feature.value;
+      feature.value_0 = (value.length === 0) ? null : 
+        value.length ? value[0] : value;
     }
   });
 
