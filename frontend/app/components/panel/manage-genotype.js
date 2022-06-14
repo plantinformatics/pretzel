8ec6922d89b33ffd212b35e30c153c325844aa1c
Don <Don.Isdale@gmail.com>
2022-06-14 14:55:12 +1000

    move vcfGenotypeLookup to added Genotypes tab.
     ... from Dataset panel : axis-brushes : axis-brush
    
    move from axis-brush.js to added manage-genotype.js : vcfGenotypeLookupDomain(), vcfGenotypeLookup().
    
    add manage-genotype.hbs, with <table> and #each brushedAxes copied from axis-brushes.hbs, and from axis-brush.hbs : copied  : <tr> datasetName, sequenceLookupDomain; moved : #if vcfGenotypeLookupDomain,   <textarea> vcfGenotypeText.
    
    mapview.hbs : add button-tab genotype / Genotypes, with manage-genotype selectedDataset
    
    add manage-genotype.js, with brushedDomainRounded () copied from axis-brush.js
    manage-genotype-test.js : as added by ember g component panel/manage-genotype

---
+++ frontend/app/components/panel/manage-genotype.js
@@ -0,0 +1,116 @@
+import Component from '@glimmer/component';
+import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+
+import { intervalSize } from '../../utils/interval-calcs';
+
+// -----------------------------------------------------------------------------
+
+const dLog = console.debug;
+
+const maxRequestInterval = 1e6;
+
+// -----------------------------------------------------------------------------
+
+
+/**
+ * @param block selected data block
+ */
+export default class PanelManageGenotypeComponent extends Component {
+  @service() controls;
+  @service() auth;
+  /** used for axisBrush.brushedAxes to instantiate axis-brush s. */
+  @service('data/flows-collate') flowsService;
+
+  @alias('controls.apiServerSelectedOrPrimary') apiServerSelectedOrPrimary;
+
+  // ---------------------------------------------------------------------------
+
+  classNames = [ 'axis-brushes' ];
+
+  @tracked
+  vcfGenotypeText = '___';
+
+  // ---------------------------------------------------------------------------
+
+  /**
+   * @return model:axis-brush
+   */
+  @computed('flowsService.axisBrush.brushedAxes')
+  get block() {
+    let blocks = this.flowsService.axisBrush.brushedAxes;
+    return blocks && blocks[0];
+  }
+
+  // ---------------------------------------------------------------------------
+
+
+  @computed('dataset')
+  get referenceDataset () {
+    let dataset = this.args.dataset;
+    return dataset;
+  }
+
+  @computed('referenceDataset')
+  get datasetName () {
+    let
+    name = this.referenceDataset?.id;
+    dLog('datasetName', name);
+    return name;
+  }
+
+
+  // ---------------------------------------------------------------------------
+
+  @computed('block.brushedDomain')
+  get brushedDomainRounded () {
+    /** copied from axis-brush.js */
+    let domain = this.block.brushedDomain;
+    if (domain) {
+      domain = domain.map((d) => d.toFixed(2));
+    }
+    return domain;
+  }
+
+  @computed('block.brushedDomain')
+  get vcfGenotypeLookupDomain () {
+    /** copied from sequenceLookupDomain() axis-brush.js
+     * could be factored to a library - probably 2 1-line functions - not compelling.
+     */
+    let
+    domain = this.block?.brushedDomain,
+    domainInteger = domain && 
+      (intervalSize(domain) <= maxRequestInterval) &&
+      domain.map((d) => d.toFixed(0));
+    return domainInteger;
+  }
+
+  // ---------------------------------------------------------------------------
+
+  vcfGenotypeLookup() {
+    let
+    domainInteger = this.vcfGenotypeLookupDomain;
+    if (domainInteger) {
+      let
+      scope = this.block?.get('block.scope'),
+      region = 'chr' + scope + ':' + domainInteger.join('-'),
+      preArgs = '-r ' + region + ' -s ExomeCapture-DAS5-003024,ExomeCapture-DAS5-003047',
+      parent = this.datasetName;
+
+      let textP = this.auth.vcfGenotypeLookup(
+        this.apiServerSelectedOrPrimary, parent, preArgs,
+        {} );
+      textP.then(
+        (text) => {
+          dLog('vcfGenotypeLookup', text);
+          this.vcfGenotypeText =  text?.sequence;
+        });
+    }
+  }
+
+  // ---------------------------------------------------------------------------
+
+}
