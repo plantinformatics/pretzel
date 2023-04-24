8f62f8cdb8ec89c3a17bfe9b2f9b3299da817f0c
Don <Don.Isdale@gmail.com>
2023-04-24 16:19:05 +1000

    show dataset colour block in un-merged rows view
    matrix-view.js : colHeaders() : use dataset.id when datasetId from columnName.split() is not defined.
    The colour block uses only dataset.id, i.e. col-Dataset- + datasetId2Class(id), so columnMeta is not required for this.
    This is extracted from feature/genotype_columnMeta a738f23f, which will not be merged at this time.

--- frontend/app/components/matrix-view.js
+++ frontend/app/components/matrix-view.js
@@ -1119,7 +1119,8 @@ export default Component.extend({
       dataset = columnName[datasetSymbol],
       /** fieldName may be sampleName, or other fields name, Ref/Alt, MAF, LD Block */
       [fieldName, datasetId] = columnName.split('\t'),
-      datasetClass = dataset ? ' col-Dataset-' + datasetId2Class(dataset.get('id')) : '',
+      datasetId_ = datasetId || (dataset && Ember_get(dataset, 'id')),
+      datasetClass = datasetId_ ? ' col-Dataset-' + datasetId2Class(datasetId_) : '',
       extraClassName = this.columnNameToClasses(fieldName);
       return '<div class="head' + extraClassName + datasetClass + '">' + fieldName + '</div>';
     });
