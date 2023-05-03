6811ba449a8857efda26e7f99d88171813e768c3
Don <Don.Isdale@gmail.com>
2023-05-03 12:08:49 +1000

    when Lookup before Samples, get Samples then Lookup
    germinate-genotype.js :
    add callSetCacheForBlockP() : request and wait for samples if name2Id is empty
    germinateGenotypeLookup() : use callSetCacheForBlockP() in place of callSetCacheForBlock(), and wait for name2IdP as well as useGerminate()

--- lb4app/lb3app/common/utilities/germinate-genotype.js
+++ lb4app/lb3app/common/utilities/germinate-genotype.js
@@ -1,3 +1,5 @@
+const util = require('util');
+
 const { Germinate } = require('./germinate');
 const { ErrorStatus } = require('./errorStatus.js');
 
@@ -54,12 +56,28 @@ function useGerminate() {
 
 /** [datasetId + scope] -> [callSetName] -> callSetDbId */
 const callSetName2DbId = {};
+/** @return sample name cache for datasetId:scope
+ */
 function callSetCacheForBlock(datasetId, scope) {
   const
   blockName = datasetId + '_' + scope,
   name2DbId = callSetName2DbId[blockName] || (callSetName2DbId[blockName] = {});
   return name2DbId;
 }
+/** if sample name cache for datasetId:scope is not empty, yield it, otherwise
+ * get samples then yield it.
+ * @return promise
+ */
+function callSetCacheForBlockP(datasetId, scope) {
+  const
+  name2DbId = callSetCacheForBlock(datasetId, scope),
+  /** if name2DbId is not empty, yield it, otherwise get samples then yield it. */
+  p = name2DbId && Object.keys(name2DbId).length ? Promise.resolve(name2DbId) : 
+    util.promisify(germinateGenotypeSamples)(datasetId, scope)
+    .then(samples => callSetCacheForBlock(datasetId, scope));
+  return p;
+}
+
 exports.germinateGenotypeSamples = germinateGenotypeSamples;
 function germinateGenotypeSamples(datasetId, scope, cb) {
   useGerminate()
@@ -79,9 +97,12 @@ function germinateGenotypeSamples(datasetId, scope, cb) {
 
 exports.germinateGenotypeLookup = germinateGenotypeLookup;
 function germinateGenotypeLookup(datasetId, scope, preArgs, nLines, undefined, cb) {
-  const fnName = 'germinateGenotypeLookup';
-  useGerminate()
-    .then((germinate) => {
+  const
+  fnName = 'germinateGenotypeLookup',
+  /** get samples for datasetId : scope if not cached. */
+  name2IdP = callSetCacheForBlockP(datasetId, scope);
+  Promise.all([name2IdP, useGerminate()])
+    .then(([name2Id, germinate]) => {
     // preArgs.samples '1-593'
     // preArgs.region : scope + ':' + domainInteger.join('-'),
     const
@@ -92,7 +113,6 @@ function germinateGenotypeLookup(datasetId, scope, preArgs, nLines, undefined, c
       [all, start, end] = match;
     }
     const
-    name2Id = callSetCacheForBlock(datasetId, scope),
     sampleNames = preArgs.samples.split('\n'),
     samplesDataP = sampleNames.map(sampleName => {
       // e.g. '1-593'
