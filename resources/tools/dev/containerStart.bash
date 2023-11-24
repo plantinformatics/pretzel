4804b77f8fc093e8993c52f0fc0bd9de7905b947
Don <Don.Isdale@gmail.com>
2023-11-24 20:53:53 +1100

    add containerStart.bash - just the template part first
    add containerStart.bash, based on source/execute conditional from aws-instances/deploy.sh

---
+++ resources/tools/dev/containerStart.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+# ------------------------------------------------------------------------------
+
+# This script file consists of bash function definitions (above), and commands (below) to run one of those functions.
+# If the script is sourced then the function definitions are added to the shell.
+# If the script is executed then the below commands are executed.
+#
+# Test if this script is executed or sourced.
+# The following actions are not performed if it is sourced;  this enables this script to be used to define the function in an interactive bash shell
+if (return 0 2>/dev/null); then : ; else
+# --------------------------------------
+
+# --------------------------------------
+fi
