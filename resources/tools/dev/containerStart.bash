d72291bff816add464f3b7b32921e73dc3d85234
Don <Don.Isdale@gmail.com>
2023-11-27 14:30:05 +1100

    Define a function containerStart. Rename containers to avoid name clash. add PRETZEL_VERSION, default stable.
    containerStart.bash :
    rename containers mongo,pretzel -> mongo_tmp,pretzel_tmp, defined via mongoContainerName, pretzelContainerName.
    before starting the mongo container check if it is already running.
    Factor handsOnTableLicenseKey into PRETZEL_DOCKER_ENV, with the value non-commercial-and-evaluation moved into a comment, so that Pretzel does not assume this value.
    name as a function : containerStart().
    add PRETZEL_VERSION, default stable.

--- resources/tools/dev/containerStart.bash
+++ resources/tools/dev/containerStart.bash
@@ -4,14 +4,20 @@
 
 # from pretzel/README.md :  [Docker on linux](#docker-on-linux)
 
-mkdir -p ~/mongodata \
- && docker run --name mongo --detach --volume ~/mongodata:/data/db --net=host mongo:5.0 \
- && until $(curl --silent --output /dev/null localhost:27017 || \
-    [ $(docker inspect -f '{{.State.Running}}' mongo) = "false" ]); do printf '.'; sleep 1; done \
- && docker run --name pretzel --detach --net=host plantinformaticscollaboration/pretzel:stable  \
- && until $(curl --silent --output /dev/null localhost:3000 || \
-    [ $(docker inspect -f '{{.State.Running}}' pretzel) = "false" ] ); do printf '.'; sleep 1; done \
- && docker logs pretzel
+mongoContainerName=mongo_tmp
+pretzelContainerName=pretzel_tmp
+
+function containerStart() {
+mkdir -p ~/Databases/Pretzel/mongodata \
+   && ( ( docker ps | fgrep $mongoContainerName ) || docker run --name $mongoContainerName --detach --volume ~/Databases/Pretzel/mongodata:/data/db --net=host mongo:5.0 ) \
+   && until $(curl --silent --output /dev/null localhost:27017 || \
+[ $(docker inspect -f '{{.State.Running}}' $mongoContainerName) = "false" ]); do printf '.'; sleep 1; done \
+   && docker run --name $pretzelContainerName ${PRETZEL_DOCKER_ENV[@]} --detach --net=host plantinformaticscollaboration/pretzel:$PRETZEL_VERSION  \
+   && until $(curl --silent --output /dev/null localhost:3000 || \
+[ $(docker inspect -f '{{.State.Running}}' $pretzelContainerName) = "false" ] ); do printf '.'; sleep 1; done  && docker logs $pretzelContainerName
+
+}
+
 
 
 # ------------------------------------------------------------------------------
@@ -25,6 +31,14 @@ mkdir -p ~/mongodata \
 if (return 0 2>/dev/null); then : ; else
 # --------------------------------------
 
+unused_var=${PRETZEL_VERSION=stable}
+export PRETZEL_VERSION
+echo PRETZEL_VERSION=$PRETZEL_VERSION
+
+# Environment variables for Pretzel server
+# e.g. PRETZEL_DOCKER_ENV=(-e handsOnTableLicenseKey=non-commercial-and-evaluation)
+
+containerStart
 
 # --------------------------------------
 fi
