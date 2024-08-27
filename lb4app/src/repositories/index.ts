81df773a3c401f18a4b7933a192550098a92e106
Don <Don.Isdale@gmail.com>
2024-08-27 20:52:01 +1000

    fixed last edit - moved content from repositories/index.ts to controllers/index.ts
    fix last edit (aider) c1e183d1 which appended a patch to repositories/index.ts instead of controllers/index.ts

--- lb4app/src/repositories/index.ts
+++ lb4app/src/repositories/index.ts
@@ -2,8 +2,3 @@ export * from './client.repository';
 export * from './group.repository';
 export * from './client-group.repository';
 export * from './dataset.repository';
-```
-
-lb4app/src/controllers/index.ts
-```typescript
-<<<<<<< SEARCH
