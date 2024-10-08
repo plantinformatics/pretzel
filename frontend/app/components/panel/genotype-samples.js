52016e89fe9ea7ddcb67fc11aed7f7d8dd153c66
Don <Don.Isdale@gmail.com>
2024-10-08 15:28:12 +1100

    in Genotype Search don't show checkbox Common samplesIntersection and remove replaced Textarea .selectedSamplesInput
    genotype-samples.hbs : wrap input checkbox Common .samplesIntersection with #if @showIntersectionCheckbox.
    
    genotype-samples.js : add trace, commented out.
    genotype-search.hbs :
     pass @showIntersectionCheckbox={{false}} to GenotypeSamples.
     drop Textarea .selectedSamplesInput .selectedSamplesText, which is replaced by the identical element in GenotypeSamples.
    
    manage-genotype.hbs : pass @showIntersectionCheckbox={{true}} to GenotypeSamples.

--- frontend/app/components/panel/genotype-samples.js
+++ frontend/app/components/panel/genotype-samples.js
@@ -2,7 +2,19 @@ import Component from '@glimmer/component';
 
 import { action } from '@ember/object';
 
+//------------------------------------------------------------------------------
+
+const dLog = console.debug;
+
+//------------------------------------------------------------------------------
+
 export default class PanelGenotypeSamplesComponent extends Component {
+
+  constructor() {
+    super(...arguments);
+
+    dLog('genotype-samples', 'showIntersectionCheckbox', this.args.showIntersectionCheckbox);
+  }
 
   @action
   vcfGenotypeSamples() {
