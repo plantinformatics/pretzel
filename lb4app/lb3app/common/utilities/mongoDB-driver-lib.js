13150c5d57721a7cd38d652148e41b80e6e9a8ff
Don-Isdale <Don.Isdale@gmail.com>
2025-08-21 11:17:03 +1000

    ensure hex string is sent in API response instead of ObjectID for blockFeaturesCount and blockFeaturesInterval
    block.js : blockFeaturesCount() and blockFeaturesInterval() : use arrayFieldToHex() to convert MongoDb ObjectID to hex string representation after reading from cache and before writing to cache; this will fix existing cache entries and ensure correct format in new stored values. It seems that ObjectIDs written to cache previously converted to hex, but now JSON.stringify() of the ObjectID is stored, and when that is read back it is not fully hydrated with .toHexString() in the .__proto__, which results in that value being sent in API response instead of the hex string format.  It is likely not necessary to apply arrayFieldToHex() to a calculated result being sent in reply because that ObjectID will still have .toHexString(), and will convert to hex string when sent in API response.
    
    mongoDB-driver-lib.js :
      drop the alias of ObjectID to ObjectId - better to stick with the library convention, rather than that of mongo shell, noted in utilities/localise-blocks.js,block-features.js
      add hexToObjectID(),  idToHex(), (the form let name = function(i) is accepted in the Web Inspector console, so is useful during initial development).
      add arrayFieldToHex().

--- lb4app/lb3app/common/utilities/mongoDB-driver-lib.js
+++ lb4app/lb3app/common/utilities/mongoDB-driver-lib.js
@@ -4,8 +4,10 @@ const { ErrorStatus } = require('./errorStatus.js');
 
 /* global require */
 /* global exports */
+/* global Buffer */
 
-var ObjectId = require('mongodb').ObjectID;
+const { ObjectID } = require('mongodb');
+
 
 /** @return true if id1 and id2 represent the same mongoDB ObjectId
  * @param id1, id2	String or ObjectId
@@ -43,5 +45,58 @@ exports.objectLookup = function(model, modelName, fnName, objectId, options) {
   return objectP;
 };
 
+
+//------------------------------------------------------------------------------
+
+/** Rehydrate a MongoDb ObjectID which has been stored in cache and read back,
+ * and hence has lost its prototype.
+ * @param {object} obj
+ * e.g. {"_bsontype":"ObjectID","id":{"0":97,"1":3,"2":221,"3":216,"4":121,"5":100,"6":79,"7":16,"8":229,"9":189,"10":66,"11":236}}
+ */
+let hexToObjectID = function (obj) {
+  const objectID = new ObjectID(Buffer.from(Object.values(obj.id)));
+  return objectID;
+};
+function x(objectID) {
+  // Convert to hex string
+  const hexString = objectID.toHexString();
+  return hexString;
+};
+// hexToId(temp1)
+
+
+/** If i has .toHexString, call it to transform it to the equivalent hex string.
+ * Otherwise if it has ._bsontype and .id, convert to a MongoDb ObjectID object, then use .toHexString().
+ * Otherwise no change.
+ * @param {string|object} i either a (hex) string, or a MongoDb ObjectID object.
+ * @return hex string representing an MongoDb ObjectID.
+ */
+let idToHex = function(i) {
+  const
+  hex = i.toHexString ? i.toHexString() :
+    (i._bsontype && i.id) ? hexToObjectID(i).toHexString() :
+    i ;
+  return hex;
+};
+/** In the given array of objects, convert id fields, identified by fieldName,
+ * to hex string representation.
+ * id values in cached results should be in hex string representation.
+ * This function is used to fix cached results, and to ensure results have ids
+ * in hex string representation before they are written to cache.
+ * Calling cb(null, result) probably converts objects using .toString(), which
+ * for id fields which are ObjectID will convert them to hex representation. But
+ * reading this value back from cache does not re-hydrate its .__proto__.
+ * So when that result is returned via cb(), it is apparently converted via
+ * JSON.stringify(), i.e. the _bsontype and array of integers is sent instead of
+ * the hex representation.
+ * @param {Array<object>} a result array; each element is an object containing
+ * [fieldname] which is some representation of an ObjectID.
+ * @param {string} fieldName	name of id field, e.g. '_id'
+ */
+exports.arrayFieldToHex =  function(a, fieldName) {
+  return a.map(({[fieldName] : i, ... e}) => ({ [fieldName] : idToHex(i), ... e}));
+};
+// arrayFieldToHex(data, '_id')
+// arrayFieldToHex(cached, '_id')
 
 //------------------------------------------------------------------------------
