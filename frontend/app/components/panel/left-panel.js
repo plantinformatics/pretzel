2d375ef208bd13ce5b6838a9ec940e443847d704
Don-Isdale <Don.Isdale@gmail.com>
2026-08-12 16:42:03 +1000

    align the Left Panel hide button with the right edge
    left-panel.js :
      add show, classNamesShown().
      toggleLeftPanel() : set(shown,  )
    
    app.scss :
      apply position: absolute to the first .panel-collapse-button (which is visible when left-panel is shown).
      To enable the first button.panel-collapse-button to use position: absolute, apply position: relative to its parent i.e. .left-split > div.
    
    left-panel.hbs : move the first button.panel-collapse-button to before the div containing <BsTab> (ul), so it can be positioned to the right edge using position: absolute.

--- frontend/app/components/panel/left-panel.js
+++ frontend/app/components/panel/left-panel.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
+import { later } from '@ember/runloop';
 
 import { htmlSafe } from '@ember/template';
 
@@ -13,6 +14,8 @@ export default Component.extend({
   style: htmlSafe('height:100%'),
   attributeBindings: ['style:style'],
   view: 'mapview',
+  /** The elements of this component are initially shown. toggled in toggleLeftPanel(). */
+  shown : true,
 
   /** Determine if the 'Selected Server' <Panel::SelectedServer> should be displayed.
    * There is no need to display it if the number of api servers is 1, i.e. only
@@ -35,11 +38,21 @@ export default Component.extend({
       return this.get('apiServers.serverSelected.datasetsBlocksOriginal') || [];
     }),
 
+  /** Define class 'shown' if .shown is true
+   * see toggleLeftPanel().
+   * May be used in future in place of jQuery $(".left-panel-shown").toggle() (also '-hidden').
+   * Currently the hbs uses :  (if this.shown '' 'hidden') instead of this.
+   */
+  classNamesShown : computed('shown', function () {
+    return this.shown ? ['shown'] : '';
+  }),
+
   actions: {
     toggleLeftPanel(shown) {
       $(".left-panel-shown").toggle();
       $(".left-panel-hidden").toggle();
       $(".left-panel-shown").trigger('toggled', [shown]);
+      later(() => this.set('shown', shown));
     },
     /** Change to the named tab.
      * @param select  this is @action select() defined in ember-bootstrap/addon/components/base/bs-tab.js
